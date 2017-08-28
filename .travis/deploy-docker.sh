80a459976d210ee2f7befa8c5a9c609af8eb9ad8
Julien Eberle <julien.eberle@epfl.ch>
2017-08-28 11:57:19 +0200

    add push to gitlab

--- .travis/deploy-docker.sh
+++ .travis/deploy-docker.sh
@@ -2,3 +2,4 @@
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" $DOCKER_REGISTRY
 docker build . -t "${DOCKER_REGISTRY}${DOCKER_REPOSITORY}renga-web-frontend:$(node -p "require('./package.json').version")"
+docker push "${DOCKER_REGISTRY}${DOCKER_REPOSITORY}renga-web-frontend:$(node -p "require('./package.json').version")"
