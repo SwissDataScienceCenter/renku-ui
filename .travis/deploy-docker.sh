95732e0de7d1027d233f67f156747d80829ca543
Julien Eberle <julien.eberle@epfl.ch>
2017-08-29 09:47:03 +0200

    add latest

--- .travis/deploy-docker.sh
+++ .travis/deploy-docker.sh
@@ -3,3 +3,6 @@
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" $DOCKER_REGISTRY
 docker build . -t "${DOCKER_REGISTRY}${DOCKER_REPOSITORY}renga-web-frontend:$(node -p "require('./package.json').version")"
 docker push "${DOCKER_REGISTRY}${DOCKER_REPOSITORY}renga-web-frontend:$(node -p "require('./package.json').version")"
+docker tag "${DOCKER_REGISTRY}${DOCKER_REPOSITORY}renga-web-frontend:$(node -p "require('./package.json').version")" "${DOCKER_REGISTRY}${DOCKER_REPOSITORY}renga-web-frontend:latest"
+docker push "${DOCKER_REGISTRY}${DOCKER_REPOSITORY}renga-web-frontend:latest"
+
