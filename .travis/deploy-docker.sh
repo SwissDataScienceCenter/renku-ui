894e51b3030bffee5051f054a182f4f1ed6a362f
Julien Eberle <julien.eberle@epfl.ch>
2017-09-13 11:36:31 +0200

    fix renaming and update versions

--- .travis/deploy-docker.sh
+++ .travis/deploy-docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" $DOCKER_REGISTRY
-docker build . -t "${DOCKER_REGISTRY}${DOCKER_REPOSITORY}renga-web-frontend:$(node -p "require('./package.json').version")"
-docker push "${DOCKER_REGISTRY}${DOCKER_REPOSITORY}renga-web-frontend:$(node -p "require('./package.json').version")"
-docker tag "${DOCKER_REGISTRY}${DOCKER_REPOSITORY}renga-web-frontend:$(node -p "require('./package.json').version")" "${DOCKER_REGISTRY}${DOCKER_REPOSITORY}renga-web-frontend:latest"
-docker push "${DOCKER_REGISTRY}${DOCKER_REPOSITORY}renga-web-frontend:latest"
+docker build . -t "${DOCKER_REGISTRY}${DOCKER_REPOSITORY}renga-ui:$(node -p "require('./package.json').version")"
+docker push "${DOCKER_REGISTRY}${DOCKER_REPOSITORY}renga-ui:$(node -p "require('./package.json').version")"
+docker tag "${DOCKER_REGISTRY}${DOCKER_REPOSITORY}renga-ui:$(node -p "require('./package.json').version")" "${DOCKER_REGISTRY}${DOCKER_REPOSITORY}renga-ui:latest"
+docker push "${DOCKER_REGISTRY}${DOCKER_REPOSITORY}renga-ui:latest"
 
