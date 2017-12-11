1b065ffdd4828e597723a5c4a5f8aa4c49d7fde2
Chandrasekhar Ramakrishnan <cramakri@ethz.ch>
2017-12-11 14:36:21 +0100

    Re-ordered inputs to ensure that our styles override bootstrap.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.min.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'font-awesome/css/font-awesome.min.css';
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
