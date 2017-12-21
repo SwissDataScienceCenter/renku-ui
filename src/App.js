2d71c8096d9519804e018fcc8ccd08c8abe19ffc
Chandrasekhar Ramakrishnan <cramakri@ethz.ch>
2017-12-21 11:40:06 +0100

    Fixed navbar collapsing breakpoint.

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ class RengaNavBar extends Component {
   render() {
     return (
       <header>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
+        <nav className="navbar navbar-expand-sm navbar-light bg-light justify-content-between">
           <span className="navbar-brand"><Link to="/"><img src={logo} alt="Renga" height="24" /></Link></span>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
