50ff92b1d85c8ebf1791c90ace1ff7d0aa3dba9a
Andreas Bleuler <andreas.bleuler@sdsc.ethz.ch>
2018-04-09 22:10:25 +0200

    Add information about starred projects to user state.

--- src/index.js
+++ src/index.js
@@ -76,7 +76,7 @@ keycloak.init()
           store.dispatch(UserState.setStarred(reducedProjects));
         });
 
-      ReactDOM.render(<App client={client} keycloak={keycloak} cookies={cookies} params={params} userState={store}/>,
+      ReactDOM.render(<App client={client} keycloak={keycloak} cookies={cookies} params={params} store={store} userState={store}/>,
         document.getElementById('root'));
     } else {
       ReactDOM.render(<AppLoggedOut keycloak={keycloak} cookies={cookies} params={params}/>,
