fc7d67f84c279917745af0c970f98a25992645f2
Andreas Bleuler <andreas.bleuler@sdsc.ethz.ch>
2018-04-18 17:13:41 +0200

    Adapt login/logout routes to removal of keycloak.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
 import { createStore } from './utils/EnhancedState';
 import 'font-awesome/css/font-awesome.min.css';
 // Use our version of bootstrap, not the one in import 'bootstrap/dist/css/bootstrap.css';
@@ -15,34 +14,49 @@ import { UserState, reducer} from './app-state';
 
 const cookies = new Cookies();
 
-const configPromise = fetch('config.json');
+const configPromise = fetch('/config.json');
 
 configPromise.then((res) => {
   res.json().then((params) => {
-    // We use a redux store to hold some global application state.
-    const store = createStore(reducer);
 
-    const client = new GitlabClient(params.GITLAB_URL + '/api/v4/', cookies.get('gitlab_token'), 'bearer');
+    if (!cookies.get('gitlab_token')){
+      ReactDOM.render(<AppLoggedOut cookies={cookies} params={params}/>,
+        document.getElementById('root'));
+    }
+    else {
 
-    // Load the user profile and dispatch the result to the store.
-    client.getUser().then(profile => {
-      store.dispatch(UserState.set(profile))
-    });
+      // We use a redux store to hold some global application state.
+      const store = createStore(reducer);
 
-    // TODO: Replace this after re-implementation of user state.
-    client.getProjects({starred: true})
-      .then((projects) => {
-        const reducedProjects = projects.map((project) => {
-          return {
-            id: project.id,
-            path_with_namespace: project.path_with_namespace
-          }});
-        store.dispatch(UserState.setStarred(reducedProjects));
+      const client = new GitlabClient(params.GITLAB_URL + '/api/v4/', cookies.get('gitlab_token'), 'bearer');
+
+      // Load the user profile and dispatch the result to the store.
+      client.getUser().then(response => {
+        // TODO: Make the api client throw exceptions on api errors.
+        if (response.message === '401 Unauthorized') {
+          ReactDOM.render(<AppLoggedOut cookies={cookies} params={params}/>,
+            document.getElementById('root'));
+        }
+        else {
+          store.dispatch(UserState.set(response))
+          // TODO: Replace this after re-implementation of user state.
+          client.getProjects({starred: true})
+            .then((projects) => {
+              const reducedProjects = projects.map((project) => {
+                return {
+                  id: project.id,
+                  path_with_namespace: project.path_with_namespace
+                }
+              });
+              store.dispatch(UserState.setStarred(reducedProjects));
+            });
+
+          ReactDOM.render(<App client={client} cookies={cookies} params={params} userState={store}/>,
+            document.getElementById('root'));
+
+          registerServiceWorker();
+        }
       });
-
-    ReactDOM.render(<App client={client} cookies={cookies} params={params} userState={store}/>,
-      document.getElementById('root'));
-
-    registerServiceWorker();
+    }
   });
 });
