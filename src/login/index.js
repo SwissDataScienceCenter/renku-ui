946c46063f4e9b1bb8c3818cc5f99c04e6afdb0b
Andreas Bleuler <andreas.bleuler@sdsc.ethz.ch>
2018-05-04 16:29:24 +0200

    Enable use of page even if user is not logged in.

--- src/login/index.js
+++ src/login/index.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
 import { parse } from 'qs'
 
 
 class Login extends Component {
-  // We first login with gitlab to get an API token and then with keycloak.
-  // The login with keycloak does not maintain the path for now.
   render() {
+    // TODO: User should be redirected to his current page by adding meaningful state here...
     window.location = this.props.params.GITLAB_URL +
       '/oauth/authorize?' +
       `client_id=${this.props.params.GITLAB_CLIENT_ID}&` +
@@ -17,10 +15,14 @@ class Login extends Component {
   }
 }
 
+
+// TODO: Currently, the user is logged out neither from gitlab nor from keycloak. Change
+// TODO: this as soon as we're talking to the gateway directly.
 class Logout extends Component {
   render() {
     this.props.cookies.remove('gitlab_token');
-    return <p>You are logged out. Back to <a href={this.props.params.BASE_URL}>Renga!</a></p>
+    window.location = this.props.params.BASE_URL;
+    return <p>logging out</p>;
   }
 }
 
@@ -28,7 +30,10 @@ class GitlabRedirect extends Component {
   render() {
     const urlParams = parse(this.props.location.hash.match(/#(.*)/)[1]);
     this.props.cookies.set('gitlab_token', urlParams.access_token, { path: '/' });
-    return <Redirect to="/"/>
+    // TODO: User should be redirected to his current page.
+    // TODO: For the moment, we do a hard reload here. Replace this by just updating application state properly.
+    window.location = this.props.params.BASE_URL;
+    return null;
   }
 }
 
