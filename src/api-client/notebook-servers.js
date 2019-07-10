SwissDataScienceCenter/renku-ui

commit 00af8778903c25ce0599edee84fd52c8d345e113

refactor(notebook): adapt to new servers endpint
The servers endpoint used for notebooks interaction has slightly changed and the server status
information are different. This refactor handles the new information properly and remove duplicate
codes from Project pages

BREAKING CHANGE: different status indication for notebook pages, according to renku-notebooks#177

fix #494

src/api-client/notebook-servers.js
@@ -44,7 +44,6 @@ function cleanAnnotations(annotations, domain) {
 
 function addNotebookServersMethods(client) {
   client.getNotebookServers = (id, branch, commit) => {
-    // TODO: add filtering logic here, remove from Notebook
     const headers = client.getBasicHeaders();
     const url = `${client.baseUrl}/notebooks/servers`;
     return client.clientFetch(url, {
@@ -55,7 +54,12 @@ function addNotebookServersMethods(client) {
       if (id) {
         // TODO: remove this filter when this API will support projectId filtering
         servers = Object.keys(servers)
-          .filter(server => servers[server].annotations["renku.io/projectId"] === id)
+          .filter(server => {
+            const annotations = cleanAnnotations(servers[server]["annotations"], "renku.io");
+            if (parseInt(annotations.projectId) === parseInt(id)) {
+              return server;
+            }
+          })
           .reduce((obj, key) => {obj[key] = servers[key]; return obj}, {});
       }
       return { "data": servers };

src/notebooks/Notebooks.state.js
@@ -92,11 +92,11 @@ class NotebooksModel extends StateModel {
     }
   }
 
-  fetchNotebooks(first) {
+  fetchNotebooks(first, projectId) {
     if (first) {
       this.setUpdating({notebooks: {all: true}});
     }
-    return this.client.getNotebookServers()
+    return this.client.getNotebookServers(projectId)
       .then(resp => {
         this.set('notebooks.all', resp.data);
         return resp.data;
@@ -115,9 +115,11 @@ class NotebooksModel extends StateModel {
       if (parseInt(annotations.projectId) !== projectId) continue;
       if (annotations["branch"] === branch.name && annotations["commit-sha"] === commit.id) {
         this.setObject({ notebooks: {
-          status: notebook.ready ?
+          status: notebook.status.ready ?
             "running" :
-            notebook.pending,
+            notebook.status.step === "Unschedulable" ?
+              "error" :
+              "pending",
           url: notebook.url
         }});
         return true;
@@ -133,11 +135,11 @@ class NotebooksModel extends StateModel {
     return false;
   }
 
-  notebookPollingIteration(verifyRunningId, projectPath) {
-    const fetchPromise = this.fetchNotebooks();
-    if (verifyRunningId) {
+  notebookPollingIteration(projectId, projectPath, first, checkRunning) {
+    const fetchPromise = this.fetchNotebooks(first, projectId);
+    if (checkRunning) {
       return fetchPromise.then((servers) => {
-        return this.verifyIfRunning(verifyRunningId, projectPath, servers);
+        return this.verifyIfRunning(projectId, projectPath, servers);
       });
     }
     else {
@@ -145,8 +147,8 @@ class NotebooksModel extends StateModel {
     }
   }
 
-  startNotebookPolling(verifyRunningId, projectPath) {
-    if (verifyRunningId) {
+  startNotebookPolling(projectId, projectPath, checkRunning) {
+    if (projectId) {
       this.setObject({notebooks: {
         status: false,
         url: null
@@ -155,12 +157,12 @@ class NotebooksModel extends StateModel {
     const oldPoller = this.get('notebooks.polling');
     if (oldPoller == null) {
       const newPoller = setInterval(() => {
-        this.notebookPollingIteration(verifyRunningId, projectPath);
+        this.notebookPollingIteration(projectId, projectPath, false, checkRunning);
       }, 3000);
       this.set('notebooks.polling', newPoller);
 
       // fetch immediatly
-      this.notebookPollingIteration(verifyRunningId, projectPath);
+      this.notebookPollingIteration(projectId, projectPath, true, checkRunning);
     }
   }
 
@@ -215,7 +217,7 @@ class NotebooksModel extends StateModel {
       }
     }
 
-    this.set('notebooks.status', 'spawn');
+    this.set('notebooks.status', 'pending');
     return this.client.startNotebook(projectPath, branch, commit, options);  
   }
 
@@ -229,16 +231,7 @@ class NotebooksModel extends StateModel {
 
   stopNotebook(serverName) {
     // manually set the state instead of waiting for the promise to resolve
-    const updatedState = {
-      notebooks: {
-        all: {
-          [serverName]: {
-            ready: false,
-            pending: "stop"
-          }
-        }
-      }
-    }
+    const updatedState = { notebooks: { all: { [serverName]: { status: { ready: false } } } } };
     this.setObject(updatedState);
     return this.client.stopNotebookServer(serverName);
   }

src/App.js
@@ -96,7 +96,7 @@ class App extends Component {
                   <Project.New key="project_new" client={this.props.client}
                     user={this.props.userState.getState().user} {...p}/> }/>
               <Route exact path="/notebooks"
-                render={p => <Notebooks key="notebooks"
+                render={p => <Notebooks key="notebooks" standalone={true}
                   user={this.props.userState.getState().user}
                   client={this.props.client} {...p} />} />
             </Switch>

src/model/RenkuModels.js
@@ -136,12 +136,6 @@ const projectSchema = new Schema({
       stop: {initial: null},
       progress: {initial: null}
     }
-  },
-  notebooks: {
-    schema: {
-      polling: {initial: null},
-      all: {initial: {}}
-    }
   }
 });
 

src/notebooks/Notebooks.container.js
@@ -33,7 +33,7 @@ import { StatusHelper } from '../model/Model'
  * @param {function} refreshBranches   Function to invoke to refresh the list of branches
  * @param {number} projectId   id of the reference project
  * @param {number} projectPath   path of the reference project
- * @param {Object[]} client   api-client used to query the gateway
+ * @param {Object} client   api-client used to query the gateway
  * @param {string} successUrl  Optional: url to redirect when then notebook is succesfully started
  * @param {Object} history  Optional: used with successUrl to properly set the new url without reloading the page
  */
@@ -260,7 +260,7 @@ class StartNotebookServer extends Component {
   }
 
   startNotebookPolling() {
-    this.model.startNotebookPolling(this.props.projectId, this.props.projectPath);
+    this.model.startNotebookPolling(this.props.projectId, this.props.projectPath, true);
   }
 
   stopNotebookPolling() {
@@ -293,6 +293,11 @@ class StartNotebookServer extends Component {
   }
 }
 
+/**
+ * @param {Object} client   api-client used to query the gateway
+ * @param {boolean} standalone   Indicates whether it's displayed as standalone
+ * @param {number} projectId   Optional, used to focus on a single project
+ */
 class Notebooks extends Component {
   constructor(props) {
     super(props);
@@ -312,7 +317,7 @@ class Notebooks extends Component {
   }
 
   startNotebookPolling() {
-    this.model.startNotebookPolling();
+    this.model.startNotebookPolling(this.props.projectId);
   }
   stopNotebookPolling() {
     this.model.stopNotebookPolling();
@@ -334,6 +339,8 @@ class Notebooks extends Component {
     return <VisibleNotebooks
       store={this.model.reduxStore}
       user={this.props.user}
+      standalone={this.props.standalone ? this.props.standalone : false}
+      projectId={this.props.projectId}
     />
   }
 }

src/notebooks/Notebooks.present.js
@@ -27,7 +27,7 @@ import { UncontrolledTooltip, UncontrolledPopover, PopoverHeader, PopoverBody } 
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import { faStopCircle, faExternalLinkAlt, faInfoCircle, faSyncAlt, faCogs } from '@fortawesome/fontawesome-free-solid';
 
-import { SpecialPropVal, StatusHelper } from '../model/Model';
+import { StatusHelper } from '../model/Model';
 import { Loader, InfoAlert, ExternalLink } from '../utils/UIComponents';
 import Time from '../utils/Time';
 import Sizes from '../utils/Media';
@@ -47,27 +47,35 @@ const Columns = {
 class NotebookServerRowAction extends Component {
   render() {
     const {status, name} = this.props;
-    const StatusText = {
-      "running": "Running",
-      "spawn": "Starting",
-      "stop": "Stopping",
-      "other": "Updating",
+    let color, statusText, interactive;
+    switch (status) {
+    case "running":
+      color = "success";
+      statusText = "Running";
+      interactive = true;
+      break;
+    case "pending":
+      color = "warning";
+      statusText = "Pending";
+      interactive = false;
+      break;
+    case "error":
+      color = "danger";
+      statusText = "Error";
+      interactive = false;
+      break;
+    default:
+      color = "danger";
+      statusText = "Unknown";
+      interactive = false;
     }
-    const color = status === "running" ? "success" :
-      status === "spawn" ? "warning" :
-        status === "stop" ? "danger" : "info";
-    const interactive = status === "running" ?
-      true :
-      false;
-    const size = this.props.small ?
-      "sm" :
-      "";
+    const size = this.props.small ? "sm" : "";
 
     if (interactive) {
       return (
         <UncontrolledButtonDropdown>
           <DropdownToggle caret color={color} disabled={!interactive} size={size}>
-            { StatusText[status] }
+            { statusText }
           </DropdownToggle>
           <DropdownMenu>
             <DropdownItem href={this.props.url} target="_blank">
@@ -87,7 +95,7 @@ class NotebookServerRowAction extends Component {
       return (
         <div>
           <Button className="d-flex" color={color} disabled size={size}>
-            { StatusText[status] }
+            { statusText }
             <Loader size="14" inline="true" margin="1" />
           </Button>
         </div>
@@ -186,11 +194,11 @@ class NotebookServerRowCompact extends Component {
 class NotebookServerRow extends Component {
   render() {
     const annotations = cleanAnnotations(this.props.annotations, "renku.io");
-    const status = this.props.ready ?
+    const status = this.props.status.ready ?
       "running" :
-      this.props.pending === "spawn" || this.props.pending === "stop" ?
-        this.props.pending :
-        "other";
+      this.props.status.step === "Unschedulable" ?
+        "error" :
+        "pending";
 
     return (
       <Media query={ Sizes.md }>
@@ -292,7 +300,7 @@ class NotebookServersList extends Component {
 class NotebookServers extends Component {
   render() {
     const serverData = this.props.servers;
-    if (!serverData || serverData === SpecialPropVal.UPDATING ) {
+    if (!serverData || StatusHelper.isUpdating(serverData)) {
       return <Loader />
     }
     return (
@@ -321,17 +329,23 @@ class NotebooksPopup extends Component {
 
 class Notebooks extends Component {
   render() {
-    const serverNumbers = Object.keys(this.props.notebooks.all).length;
+    const { standalone } = this.props;
+    let title, popup = null;
+    if (standalone) {
+      title = (<h1>Notebooks</h1>);
+      const serverNumbers = Object.keys(this.props.notebooks.all).length;
+      popup = (<NotebooksPopup servers={serverNumbers} />);
+    }
+
     return <Row>
       <Col>
-        <h1>
-          Notebooks
-        </h1>
+        {title}
         <NotebookServers
           servers={this.props.notebooks.all}
           stop={this.props.handlers.onStopNotebook}
+          projectId={this.props.projectId}
         />
-        <NotebooksPopup servers={serverNumbers} />
+        {popup}
       </Col>
     </Row>
   }
@@ -342,7 +356,7 @@ class StartNotebookServer extends Component {
     return (
       <Row>
         <Col xs={12} sm={10} md={8} lg={6}>
-          <h3>Start new Jupyterlab server</h3>
+          <h3>Start new JupyterLab server</h3>
           <Form>
             <StartNotebookBranches {...this.props} />
             <StartNotebookCommits {...this.props} />
@@ -552,7 +566,7 @@ class StartNotebookOptions extends Component {
     }
     const { justStarted } = this.props;
     if (justStarted) {
-      return <Label>Starting new Jupyterlab server... <Loader size="14" inline="true" /></Label>
+      return <Label>Starting new JupyterLab server... <Loader size="14" inline="true" /></Label>
     }
     const { status, url } = this.props.notebooks;
     let content;
@@ -579,30 +593,24 @@ class StartNotebookOptions extends Component {
       if (status === "running") {
         content = (
           <FormGroup>
-            <Label>A Jupyterlab server is already running.</Label>
+            <Label>A JupyterLab server is already running.</Label>
             <br />
             <ExternalLink url={url} title="Connect" />
           </FormGroup>
         );
       }
-      else if (status === "spawn") {
+      else if (status === "pending") {
         content = (
           <FormGroup>
-            <Label>A Jupyterlab server is already starting, please wait...</Label>
-          </FormGroup>
-        );
-      }
-      else if (status === "stop") {
-        content = (
-          <FormGroup>
-            <Label>A Jupyterlab server is stopping, please wait...</Label>
+            <Label>A JupyterLab server for this commit is starting or terminating, please wait...</Label>
           </FormGroup>
         );
       }
       else {
         content = (
           <FormGroup>
-            <Label>A Jupyterlab server is already running but it is currently not available. Please wait...</Label>
+            <Label>A JupyterLab server is already running, but it is currently not available.
+              You can check its status from the Notebooks page</Label>
           </FormGroup>
         );
       }

src/project/Project.js
@@ -29,7 +29,7 @@ import { connect } from 'react-redux'
 import { StateKind } from '../model/Model';
 import Present from './Project.present'
 
-import { ProjectModel, GraphIndexingStatus, PollingInterval } from './Project.state'
+import { ProjectModel, GraphIndexingStatus } from './Project.state'
 import Ku from '../ku/Ku'
 import { FileLineage } from '../file'
 import { ACCESS_LEVELS } from '../api-client';
@@ -69,11 +69,6 @@ class View extends Component {
   async fetchModifiedFiles() { return this.projectState.fetchModifiedFiles(this.props.client, this.props.id); }
   async fetchBranches() { return this.projectState.fetchBranches(this.props.client, this.props.id); }
   async fetchCIJobs() { return this.projectState.fetchCIJobs(this.props.client, this.props.id); }
-  async startNotebookServersPolling() {
-    return this.projectState.startNotebookServersPolling(this.props.client, this.props.id,PollingInterval.START);
-  }
-  async stopNotebookServersPolling() { return this.projectState.stopNotebookServersPolling(); }
-  async stopNotebookServer(serverName) { return this.projectState.stopNotebookServer(this.props.client, serverName, this.props.id); }
   async createGraphWebhook() { return this.projectState.createGraphWebhook(this.props.client, this.props.id); }
   async stopCheckingWebhook() { this.projectState.stopCheckingWebhook(); }
   async fetchGraphWebhook() { this.projectState.fetchGraphWebhook(this.props.client, this.props.id, this.props.user); }
@@ -311,15 +306,6 @@ class View extends Component {
       this.setProjectOpenFolder(filePath);
     },
     fetchCIJobs: () => { this.fetchCIJobs() },
-    startNotebookServersPolling: () => {
-      this.startNotebookServersPolling();
-    },
-    stopNotebookServersPolling: () => {
-      this.stopNotebookServersPolling();
-    },
-    stopNotebookServer: (serverName) => {
-      this.stopNotebookServer(serverName);
-    },
     createGraphWebhook: (e) => { 
       e.preventDefault();
       return this.createGraphWebhook();

src/project/Project.present.js
@@ -47,7 +47,7 @@ import { ExternalLink, Loader, RenkuNavLink, TimeCaption} from '../utils/UICompo
 import { InfoAlert, SuccessAlert, WarnAlert, ErrorAlert } from '../utils/UIComponents'
 import { SpecialPropVal } from '../model/Model'
 import { ProjectTags, ProjectTagList } from './shared'
-import { NotebookServers, StartNotebookServer } from '../notebooks'
+import { Notebooks, StartNotebookServer } from '../notebooks'
 import FilesTreeView from './filestreeview/FilesTreeView';
 import { ACCESS_LEVELS } from '../api-client';
 
@@ -579,14 +579,6 @@ class ProjectViewFiles extends Component {
 }
 
 class ProjectNotebookServers extends Component {
-  componentDidMount() {
-    this.props.startNotebookServersPolling();
-  }
-
-  componentWillUnmount() {
-    this.props.stopNotebookServersPolling();
-  }
-
   render() {
     let launch = null;
     if (this.props.visibility.accessLevel >= ACCESS_LEVELS.DEVELOPER) {
@@ -600,11 +592,7 @@ class ProjectNotebookServers extends Component {
 
     return (
       <Col xs={12}>
-        <NotebookServers
-          servers={ this.props.notebooks.all }
-          stop= { this.props.stopNotebookServer }
-          projectId={this.props.id}
-        />
+        <Notebooks projectId={this.props.id} client={this.props.client} standalone={false} />
         {launch}
       </Col>
     )

commit fd131562b92368ecb14d0e2a9cde7c3dbc32c483

fix(notebook): fix errors introduced by #498
#498 introduced a bug in Notebook.state since the polling actions are never interrupted when needed
-- e.g. when the query to the servers endpoint is skipped or the list of servers is still updating

src/api-client/notebook-servers.js
@@ -59,6 +59,7 @@ function addNotebookServersMethods(client) {
             if (parseInt(annotations.projectId) === parseInt(id)) {
               return server;
             }
+            return null;
           })
           .reduce((obj, key) => {obj[key] = servers[key]; return obj}, {});
       }

src/notebooks/Notebooks.state.js
@@ -25,6 +25,7 @@
 
 import { Schema, StateKind, StateModel } from '../model/Model';
 import { cleanAnnotations } from '../api-client/notebook-servers';
+import { StatusHelper } from '../model/Model'
 
 const notebooksSchema = new Schema({
   notebooks: {
@@ -118,6 +119,7 @@ class NotebooksModel extends StateModel {
     const notebooks = servers ?
       servers :
       this.get('notebooks.all');
+    if (StatusHelper.isUpdating(notebooks)) return;
     const branch = this.get('filters.branch');
     const commit = this.get('filters.commit');
     for (let notebookName of Object.keys(notebooks)) { 
@@ -148,6 +150,7 @@ class NotebooksModel extends StateModel {
 
   notebookPollingIteration(projectId, projectPath, first, checkRunning) {
     const fetchPromise = this.fetchNotebooks(first, projectId);
+    if (!fetchPromise) return;
     if (checkRunning) {
       return fetchPromise.then((servers) => {
         return this.verifyIfRunning(projectId, projectPath, servers);
@@ -249,3 +252,4 @@ class NotebooksModel extends StateModel {
 }
 
 export default NotebooksModel;
+