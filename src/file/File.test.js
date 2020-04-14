30d59dd7b6c9da4b60de25de3d354c2fe71b116b
Chandrasekhar Ramakrishnan <cramakri@ethz.ch>
2020-04-15 00:47:09 +0200

    feat: provide UI for hiding/showing code cells (#870)
    Provide a switch for changing code visibility.
    
    Fix #838

--- src/file/File.test.js
+++ src/file/File.test.js
@@ -31,7 +31,7 @@ import { testClient as client } from "../api-client";
 import { generateFakeUser } from "../user/User.test";
 import { ShowFile, JupyterButton } from "./index";
 import { StateModel, globalSchema } from "../model";
-import { tweakCellMetadata } from "./File.present";
+import { NotebookSourceDisplayMode, tweakCellMetadata } from "./File.present";
 
 const model = new StateModel(globalSchema);
 
@@ -210,5 +210,116 @@ describe("cell metadata messaging", () => {
     expect(result.cells[0]).toEqual(notebook.cells[0]);
     expect(notebook.cells[1].metadata.hide_input).toEqual(undefined);
     expect(result.cells[1].metadata.hide_input).toEqual(true);
+  });
+
+  const modeNotebook = {
+    cells: [
+      {
+        cell_type: "code",
+        execution_count: 3,
+        metadata: {},
+        outputs: [
+          {
+            name: "stdout",
+            output_type: "stream",
+            text: ["show input\n"]
+          }
+        ],
+        source: ['print("show input")']
+      },
+      {
+        cell_type: "code",
+        execution_count: 4,
+        metadata: {
+          jupyter: {
+            source_hidden: true
+          },
+          papermill: {
+            duration: 0.48317,
+            end_time: "2020-03-31T08:27:03.045655",
+            exception: false,
+            start_time: "2020-03-31T08:27:02.562485",
+            status: "completed"
+          },
+          tags: []
+        },
+        outputs: [
+          {
+            name: "stdout",
+            output_type: "stream",
+            text: ["hide input\n"]
+          }
+        ],
+        source: ['print("hide input")']
+      },
+      {
+        cell_type: "code",
+        execution_count: 5,
+        metadata: {
+          hide_input: true,
+          papermill: {
+            duration: 0.48317,
+            end_time: "2020-03-31T08:27:03.045655",
+            exception: false,
+            start_time: "2020-03-31T08:27:02.562485",
+            status: "completed"
+          },
+          tags: []
+        },
+        outputs: [
+          {
+            name: "stdout",
+            output_type: "stream",
+            text: ["hide too\n"]
+          }
+        ],
+        source: ['print("hide too")']
+      }
+    ],
+    metadata: {
+      hide_input: true,
+      kernelspec: {
+        display_name: "Python 3",
+        language: "python",
+        name: "python3"
+      },
+      language_info: {
+        codemirror_mode: {
+          name: "ipython",
+          version: 3
+        },
+        file_extension: ".py",
+        mimetype: "text/x-python",
+        name: "python",
+        nbconvert_exporter: "python",
+        pygments_lexer: "ipython3",
+        version: "3.7.4"
+      }
+    },
+    nbformat: 4,
+    nbformat_minor: 4
+  };
+
+  it("handles default mode correctly", () => {
+    const result = tweakCellMetadata(modeNotebook, NotebookSourceDisplayMode.DEFAULT);
+    expect(result.cells[0]).toEqual(modeNotebook.cells[0]);
+    expect(modeNotebook.cells[1].metadata.hide_input).toEqual(undefined);
+    expect(result.cells[1].metadata.hide_input).toEqual(true);
+  });
+  it("handles hidden mode correctly", () => {
+    const result = tweakCellMetadata(modeNotebook, NotebookSourceDisplayMode.HIDDEN);
+    expect(modeNotebook.cells[0].metadata.hide_input).toEqual(undefined);
+    expect(result.cells[0].metadata.hide_input).toEqual(true);
+    expect(modeNotebook.cells[1].metadata.hide_input).toEqual(undefined);
+    expect(result.cells[1].metadata.hide_input).toEqual(true);
+  });
+  it("handles shown mode correctly", () => {
+    const result = tweakCellMetadata(modeNotebook, NotebookSourceDisplayMode.SHOWN);
+    expect(modeNotebook.cells[0].metadata.hide_input).toEqual(undefined);
+    expect(result.cells[0].metadata.hide_input).toEqual(false);
+    expect(modeNotebook.cells[1].metadata.hide_input).toEqual(undefined);
+    expect(result.cells[1].metadata.hide_input).toEqual(false);
+    expect(modeNotebook.metadata.hide_input).toEqual(true);
+    expect(result.metadata.hide_input).toEqual(false);
   });
 });
