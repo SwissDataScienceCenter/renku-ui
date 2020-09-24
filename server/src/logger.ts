b0c09147f7b1eb2baf3946e5c5a56539c81fd279
Chandrasekhar Ramakrishnan <cramakri@ethz.ch>
2020-09-24 17:12:11 +0200

    feat(server): initial express/typescript server (#225) (#1043)
    - winston for logging
    - nodemon and concurrently for rebuild on edit
    - use morgan for logging of requests

---
+++ server/src/logger.ts
@@ -0,0 +1,34 @@
+/*!
+ * Copyright 2020 - Swiss Data Science Center (SDSC)
+ * A partnership between École Polytechnique Fédérale de Lausanne (EPFL) and
+ * Eidgenössische Technische Hochschule Zürich (ETHZ).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//
+// Global logging support
+//
+
+import winston from "winston";
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  defaultMeta: { service: "renku-ui-server" },
+  transports: [
+    new winston.transports.Console({ format: winston.format.simple() })
+  ]
+});
+
+export default logger;
---
+++ server/src/routes/index.ts
@@ -0,0 +1,43 @@
+/*!
+ * Copyright 2020 - Swiss Data Science Center (SDSC)
+ * A partnership between École Polytechnique Fédérale de Lausanne (EPFL) and
+ * Eidgenössische Technische Hochschule Zürich (ETHZ).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import express from "express";
+
+function register(app: express.Application) : void {
+  // define a route handler for the default home page
+  app.get( "/", ( req, res ) => {
+    res.send( "Hello world!" );
+  } );
+
+  // define a route handler for the readiness probe
+  app.get( "/readiness", ( req, res ) => {
+    res.send( "ready" );
+  } );
+
+  // define a route handler for the liveness probe
+  app.get( "/liveness", ( req, res ) => {
+    res.send( "live" );
+  } );
+
+  // requests through the gateway are routed to /ui/
+  app.get( "/ui/", ( req, res ) => {
+    res.send( "Hello ui!" );
+  } );
+}
+
+export default { register };
