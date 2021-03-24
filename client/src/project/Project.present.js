e24a79a7995247352f9becab8888eee7c3d9828b
Chandrasekhar Ramakrishnan <ciyer@users.noreply.github.com>
2021-03-24 09:45:13 +0100

    feat: show clone command first on project settings (#1283) (#1284)

--- client/src/project/Project.present.js
+++ client/src/project/Project.present.js
@@ -1065,7 +1065,7 @@ function RepositoryUrlRow(props) {
 class RepositoryUrls extends Component {
   render() {
     return (
-      <Fragment>
+      <div>
         <Label className="font-weight-bold">Repository URL</Label>
         <Table size="sm">
           <tbody>
@@ -1073,7 +1073,7 @@ class RepositoryUrls extends Component {
             <RepositoryUrlRow urlType="HTTP" url={this.props.system.http_url} />
           </tbody>
         </Table>
-      </Fragment>
+      </div>
     );
   }
 }
@@ -1096,11 +1096,11 @@ function GitCloneCmd(props) {
   const gitClone = `git clone ${externalUrl}.git && cd ${projectPath} && git lfs install --local --force`;
   const gitHooksInstall = "renku githooks install"; // eslint-disable-line
   return (cmdOpen) ?
-    <div style={{ fontSize: "smaller" }} className="mt-3">
-      <p className="font-italic">
+    <div className="mt-3">
+      <p style={{ fontSize: "smaller" }} className="font-italic">
         If the <b>renku</b> command is not available, you can clone a project using Git.
       </p>
-      <Table size="sm" className="mb-0" borderless={true}>
+      <Table style={{ fontSize: "smaller" }} size="sm" className="mb-0" borderless={true}>
         <tbody>
           <tr>
             <th scope="row">Git<sup>*</sup></th>
@@ -1122,7 +1122,7 @@ function GitCloneCmd(props) {
           </tr>
         </tbody>
       </Table>
-      <Button color="link" onClick={() => setCmdOpen(false)}>
+      <Button style={{ fontSize: "smaller" }} color="link" onClick={() => setCmdOpen(false)}>
         Hide git command
       </Button>
     </div> :
@@ -1138,7 +1138,7 @@ class RepositoryClone extends Component {
     const { externalUrl } = this.props;
     const renkuClone = `renku clone ${externalUrl}.git`;
     return (
-      <Fragment>
+      <div>
         <Label className="font-weight-bold">Clone commands</Label>
         <Table size="sm" className="mb-0">
           <tbody>
@@ -1146,7 +1146,7 @@ class RepositoryClone extends Component {
           </tbody>
         </Table>
         <GitCloneCmd externalUrl={externalUrl} projectPath={this.props.core.project_path} />
-      </Fragment>
+      </div>
     );
   }
 }
@@ -1199,8 +1199,8 @@ class ProjectSettings extends Component {
           <ProjectDescription {...this.props} />
         </Col>
         <Col xs={12} lg={6}>
-          <RepositoryUrls {...this.props} />
           <RepositoryClone {...this.props} />
+          <RepositoryUrls {...this.props} />
         </Col>
       </Row>
     </Col>;
