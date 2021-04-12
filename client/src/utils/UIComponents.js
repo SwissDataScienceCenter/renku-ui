22aae5eac865e357f46105e26195ec19834c40e9
Virginia Friedrich <42647877+vfried@users.noreply.github.com>
2021-04-12 11:24:47 +0200

    feat: redesign of environments (#1291)

--- client/src/utils/UIComponents.js
+++ client/src/utils/UIComponents.js
@@ -746,16 +746,17 @@ function ButtonWithMenu(props) {
   const toggleOpen = () => setOpen(!dropdownOpen);
   const size = (props.size) ? props.size : "md";
 
-  return <ButtonDropdown 
-    size={size} 
-    isOpen={dropdownOpen} 
+  return <ButtonDropdown
+    size={size}
+    isOpen={dropdownOpen}
     toggle={toggleOpen}
+    color={props.color || "primary"}
     direction={props.direction}
     disabled={props.disabled}
-    >
+  >
     {props.default}
-    <DropdownToggle color="primary" className="alternateToggleStyle">
-      <FontAwesomeIcon icon={faEllipsisV} style={{ color: "white", backgroundColor: "#5561A6" }} />
+    <DropdownToggle color={props.color || "primary"}>
+      <FontAwesomeIcon icon={faEllipsisV}/>
     </DropdownToggle>
     <DropdownMenu end>
       {props.children}
