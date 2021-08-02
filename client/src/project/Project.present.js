17101f3e02b5152747e3eacaae8ac9369fc75119
Virginia Friedrich <virginiafriedrich@gmail.com>
2021-08-02 14:03:34 +0200

    feat(files): adjustable width in files disp

--- client/src/project/Project.present.js
+++ client/src/project/Project.present.js
@@ -917,19 +917,19 @@ class ProjectViewFiles extends Component {
   }
 
   render() {
-    return [
-      <Col key="files" md={4} lg={3} xl={2}>
+    return <div className="d-flex">
+      <div key="files" className="variableWidthColLeft pe-2 ps-2">
         <ProjectFilesNav {...this.props} />
-      </Col>,
-      <Col key="content" md={8} lg={9} xl={10}>
+      </div>
+      <div key="content" className="flex-shrink-1 variableWidthColRight pe-2 ps-2">
         <Switch>
           <Route path={this.props.lineageUrl}
             render={p => this.props.lineageView(p)} />
           <Route path={this.props.fileContentUrl}
             render={props => this.props.fileView(props)} />
         </Switch>
-      </Col>
-    ];
+      </div>
+    </div>;
   }
 }
 
