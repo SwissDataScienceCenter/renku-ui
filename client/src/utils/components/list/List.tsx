e84f0337e0a4ac0f09a68c23ea44d710e237a057
Andrea Cordoba <dandrea.cordoba@gmail.com>
2022-05-10 23:14:47 -0500

    feat: add entity component for entity search result

---
+++ client/src/utils/components/list/List.tsx
@@ -0,0 +1,102 @@
+/*!
+ * Copyright 2022 - Swiss Data Science Center (SDSC)
+ * A partnership between École Polytechnique Fédérale de Lausanne (EPFL) and
+ * Eidgenössische Technische Hochschule Zürich (ETHZ).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import * as React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGlobe, faLock, faPlay, faPlus, faUserFriends } from "@fortawesome/free-solid-svg-icons";
+import "./ListCard.css";
+import { toCapitalized } from "../../helpers/HelperFunctions";
+import { Button } from "../../ts-wrappers";
+import {
+  EntityType,
+  ListElementProps,
+  ListDisplayType,
+  VisibilityIconProps,
+  EntityIconProps,
+  EntityButtonProps
+} from "./List.d";
+import ListCard from "./ListCard";
+import ListBar from "./ListBar";
+
+export const VisibilityIcon = ({ visibility }: VisibilityIconProps) => {
+  if (!visibility) return null;
+  const icon = {
+    public: <FontAwesomeIcon icon={faGlobe} />,
+    private: <FontAwesomeIcon icon={faLock} />,
+    internal: <FontAwesomeIcon icon={faUserFriends} />
+  };
+  return <span className="visibility-icon text-rk-text" title={toCapitalized(visibility)}>
+    { icon[visibility] || "" }
+  </span>;
+};
+
+
+export const EntityIcon = ({ entityType }: EntityIconProps) => {
+  if (!entityType)
+    return null;
+
+  const entityIcon = entityType === EntityType.Project ? "/project-icon.svg" : "/dataset-icon.svg";
+  return <div><img width={24} src={entityIcon} /></div>;
+};
+
+
+export const EntityButton = ({ entityType, handler }: EntityButtonProps) => {
+  if (!entityType)
+    return null;
+
+  const onMainButton = () => {
+    if (handler)
+      handler();
+  };
+
+  return entityType === EntityType.Project ?
+    (<Button key="start-session" color="primary" onClick={onMainButton}>
+      <FontAwesomeIcon className="me-1" icon={faPlay} /> Start session
+    </Button>) :
+    (<Button key="add-to-project" color="primary" onClick={onMainButton}>
+      <FontAwesomeIcon className="me-1" icon={faPlus} /> Add to project
+    </Button>);
+};
+
+/**
+ * ListCard/ListBar returns a card or a bar displaying an item in a List.
+ *
+ * @param props.type Type of visualization (Card, Bar).
+ * @param props.url containing a link to the entity details.
+ * @param props.title title of the entity.
+ * @param props.slug slug of the entity.
+ * @param props.description description of the entity.
+ * @param props.visibility visibility of the entity.
+ * @param props.tagList tag list of the entity.
+ * @param props.timeCaption date to put inside the time caption of the entity.
+ * @param props.labelCaption label to put inside the time caption of the entity, if empty defaults to Updated.
+ * @param props.mediaContent image of the entity.
+ * @param props.creators creators of the entity, if more than 3 they will be cropped at 3.
+ * @param props.itemType type of the entity being rendered, the color of the circle depends on this.
+ * @param props.handler handler function for main button.
+ */
+function List(props: ListElementProps) {
+  const type = props.type;
+
+  return type === ListDisplayType.Card ? (
+    <ListCard {...props} />
+  ) : (
+    <ListBar {...props} />
+  );
+}
+
+export default List;
---
+++ client/src/utils/components/list/ListBar.tsx
@@ -0,0 +1,106 @@
+/*!
+ * Copyright 2022 - Swiss Data Science Center (SDSC)
+ * A partnership between École Polytechnique Fédérale de Lausanne (EPFL) and
+ * Eidgenössische Technische Hochschule Zürich (ETHZ).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import * as React from "react";
+import { Col } from "../../ts-wrappers";
+import { ProjectTagList } from "../../../project/shared";
+import { TimeCaption } from "../TimeCaption";
+import { ListElementProps } from "./List.d";
+import "./ListBar.css";
+import { EntityButton, EntityIcon, VisibilityIcon } from "./List";
+const Link = require("react-router-dom").Link;
+
+function ListBar(
+  { url,
+    title,
+    description,
+    tagList,
+    timeCaption,
+    labelCaption,
+    mediaContent,
+    creators,
+    slug,
+    handler,
+    itemType,
+    visibility
+  }: ListElementProps) {
+
+  const mediaContentBox = mediaContent ?
+    (
+      <Col className="d-flex pt-4 flex-shrink-0 media-box" >
+        {mediaContent ?
+          <img src={mediaContent} width={100} alt="dataset image" className="card-img-bottom"/>
+          : null
+        }
+      </Col>
+    ) : null;
+  return (<Link className="d-flex flex-row rk-search-result" to={url}>
+    <div className="me-3 mt-2 d-flex flex-column align-items-center icons-box">
+      <EntityIcon entityType={itemType}/>
+      <div className="mt-4">
+        <VisibilityIcon visibility={visibility}/>
+      </div>
+    </div>
+    <Col className="d-flex align-items-start flex-column overflow-hidden">
+      <div className="title d-inline-block text-truncate">
+        {title}
+        {
+          slug ?
+            <span className="slug font-weight-light text-rk-text ms-2">
+              {slug}
+            </span>
+            : null
+        }
+      </div>
+      {
+        creators ?
+          <div className="creators text-truncate text-rk-text">
+            <small style={{ display: "block" }} className="font-weight-light">
+              {creators.slice(0, 3).map((creator) => creator.name).join(", ")}
+              {creators.length > 3 ? ", et al." : null}
+            </small>
+          </div>
+          : null
+      }
+      {
+        timeCaption ?
+          <div className="mt-auto">
+            <TimeCaption caption={labelCaption || "Updated"} time={timeCaption} className="text-secondary"/>
+          </div>
+          : null
+      }
+      <div className="mt-3">
+        {description}
+      </div>
+      {
+        tagList ?
+          <div className="tagList py-2">
+            <ProjectTagList tagList={tagList} />
+          </div>
+          : null
+      }
+    </Col>
+    {mediaContentBox}
+    <Col className="d-flex flex-column flex-shrink-0 button-box">
+      <div className="card-footer-left py-4">
+        <EntityButton entityType={itemType} handler={handler} />
+      </div>
+    </Col>
+  </Link>);
+}
+
+export default ListBar;
