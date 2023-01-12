e669aa622907ad2c18dc0fe0f9f5cf33c739ef60
Chandrasekhar Ramakrishnan <cramakri@ethz.ch>
2023-01-12 12:55:48 +0100

    build: update storybook (#2264) (#2297)

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,22 +1,20 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app",
-  ],
+  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/preset-create-react-app"],
   "framework": "@storybook/react",
   typescript: {
     reactDocgen: 'react-docgen-typescript',
     reactDocgenTypescriptOptions: {
       compilerOptions: {
         allowSyntheticDefaultImports: false,
-        esModuleInterop: false,
-      },
+        esModuleInterop: false
+      }
     }
+  },
+  core: {
+    builder: "@storybook/builder-webpack5"
+  },
+  docs: {
+    autodocs: true
   }
-}
+};
