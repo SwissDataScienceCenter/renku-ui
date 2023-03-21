af6a44b73ea798ac83ad07a29932511953c601b8
Lorenzo Cavazzi <43481553+lorenzo-cavazzi@users.noreply.github.com>
2023-03-21 15:37:53 +0100

    test(e2e): reorganize e2e tests (#2431)
    * Remove `live` tests.
    * Move the e2e tests to the CI pipeline that doesn't require a full RenkuLab deployment.
    * Refresh the Readme file.

--- tests/cypress/e2e/newSession.spec.ts
+++ tests/cypress/e2e/newSession.spec.ts
@@ -16,9 +16,9 @@
  * limitations under the License.
  */
 
-import Fixtures from "../../support/renkulab-fixtures";
-import "../../support/utils";
-import "../../support/sessions/gui_commands";
+import Fixtures from "../support/renkulab-fixtures";
+import "../support/utils";
+import "../support/sessions/gui_commands";
 
 describe("launch sessions", () => {
   const fixtures = new Fixtures(cy);
