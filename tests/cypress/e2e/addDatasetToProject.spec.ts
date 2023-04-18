410343c058ab1ef7029c912f09e400bd561b2e9e
Johann-Michael Thiebaut <johann.thiebaut@gmail.com>
2023-04-18 14:05:03 +0200

    restore e2e

--- tests/cypress/e2e/addDatasetToProject.spec.ts
+++ tests/cypress/e2e/addDatasetToProject.spec.ts
@@ -141,7 +141,7 @@ describe("Add dataset to new project", () => {
 
 });
 
-describe.only("Invalid dataset", () => {
+describe("Invalid dataset", () => {
   const fixtures = new Fixtures(cy);
   fixtures.useMockedData = Cypress.env("USE_FIXTURES") === true;
   beforeEach(() => {
