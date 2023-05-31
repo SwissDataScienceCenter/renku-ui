2045d617766fb13f1050f6b059eeda1948f55e09
Johann-Michael Thiebaut <johann.thiebaut@sdsc.ethz.ch>
2023-05-31 10:41:09 +0200

    Update PR#2550

--- client/src/components/loginAlert/LoginAlert.tsx
+++ client/src/components/loginAlert/LoginAlert.tsx
@@ -32,10 +32,10 @@ import { Url } from "../../utils/helpers/url";
 export interface LoginAlertProps {
   logged: boolean;
   noWrapper?: boolean;
-  textIntro: string;
-  textLogin: string;
-  textPost: string;
-  textPre: string;
+  textIntro?: string;
+  textLogin?: string;
+  textPost?: string;
+  textPre?: string;
 }
 
 const LoginAlert = ({
