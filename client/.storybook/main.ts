97eb00d63be1a840655c874c36b366e18be1c871
Andrea Cordoba <dandrea.cordoba@gmail.com>
2023-08-16 10:00:45 +0200

    migrate storybook to v7.3

---
+++ client/.storybook/main.ts
@@ -0,0 +1,40 @@
+import { StorybookConfig } from "@storybook/react-webpack5";
+const config: StorybookConfig = {
+  // stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/components/authorFilter/AuthorFilter.stories.tsx"],
+
+  addons: [
+    // "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    // "@storybook/addon-interactions",
+    "@storybook/preset-create-react-app",
+    // "addon-redux",
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        configureJSX: true,
+      },
+    },
+  ],
+
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      compilerOptions: {
+        allowSyntheticDefaultImports: false,
+        esModuleInterop: false,
+      },
+    },
+  },
+
+  docs: {
+    autodocs: true,
+  },
+};
+
+export default config;
--- client/.storybook/store.js
+++ client/.storybook/store.js
@@ -1,4 +1,4 @@
-import { createStore } from "../src/utils/helpers/EnhancedState.js";
+import { createStore } from "../src/utils/helpers/EnhancedState.ts";
 import { enhancer as withReduxEnhancer } from "addon-redux";
 
 const store = createStore({}, [withReduxEnhancer]);
