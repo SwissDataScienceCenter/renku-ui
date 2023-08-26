57bd9635af7657b8da40976a43448ca78d55b0f3
Andrea Cordoba <dandrea.cordoba@gmail.com>
2023-08-26 13:21:35 +0200

    add suggestions in Button story

--- client/src/components/buttons/Buttons.stories.tsx
+++ client/src/components/buttons/Buttons.stories.tsx
@@ -29,20 +29,28 @@ interface LabelsProps {
 const componentDescription = `
   Custom Buttons for actions in forms, cards, and more.
   Utilize a defined color palette for various states and entities.
-  - **Entities:** Projects, Datasets y Workflows
+  - **Entities:** Projects, Datasets and Workflows
   - **States:** Error, Information, Alert
  
 <details>
 <summary>Best Practices</summary>
 <ul>
+<li>Use labels that accurately describe the action the button performs. Frame the label as an action the user is taking, such as "Submit," "Save," "Cancel," "Delete," "Edit," etc.</li>
+<li>Ensure that the design and color of the button align with the context in which it's used. For example, use primary buttons for main actions and secondary buttons for less prominent actions.</li>
+<li>Don't overload a single page or section with too many buttons. Prioritize the most essential actions to avoid overwhelming users.</li>
 <li>For modals and single-page forms right-align buttons with the container.</li>
 <li>Always place the primary button on the right, the secondary button just to the left of it.</li>
 </ul>
 </details>
+
+###ReactstrapComponents
+This component uses Button from reactstrap internally. So please take a look also there at their extensive documentation.
+
 `;
 const meta: Meta<typeof Button> = {
   component: Button,
-  title: "components/Buttons",
+  title: "components/Buttons/Normal Buttons",
+
   parameters: {
     docs: {
       description: {
@@ -61,12 +69,13 @@ export const Primary: Story = (args: LabelsProps) => (
   </>
 );
 Primary.args = {
-  text: "My Button",
+  text: "Create Project",
 };
 Primary.parameters = {
   docs: {
     description: {
-      story: "This is the primary button variant, suitable for key actions.",
+      story:
+        "This is the primary button variant. Use this for the main action.",
     },
   },
 };
@@ -77,13 +86,13 @@ export const Secondary: Story = (args: LabelsProps) => (
   </>
 );
 Secondary.args = {
-  text: "My Button",
+  text: "Cancel",
 };
 Secondary.parameters = {
   docs: {
     description: {
       story:
-        "The secondary button is used for secondary or less emphasized actions.",
+        "The secondary button is used for actions we don't want to emphasize too much (E.G: a `Cancel` button in a modal) or actions that are not required (E.G: `Add Variable` in the new session page to add an optional environment variable)",
     },
   },
 };
@@ -97,33 +106,13 @@ export const WithIcon: Story = (args: LabelsProps) => (
   </>
 );
 WithIcon.args = {
-  text: "My Button",
+  text: "Edit Project",
 };
 WithIcon.parameters = {
   docs: {
     description: {
       story:
-        "This variation when include icon, place icon to the left of the text",
-    },
-  },
-};
-
-export const SecondaryWithIcon = (args: LabelsProps) => (
-  <>
-    <Button className="btn-outline-rk-green btn-icon-text">
-      <FontAwesomeIcon icon={faPen} color="dark" />
-      {args.text}
-    </Button>
-  </>
-);
-SecondaryWithIcon.args = {
-  text: "My Button",
-};
-SecondaryWithIcon.parameters = {
-  docs: {
-    description: {
-      story:
-        "This variation when include icon, place icon to the left of the text",
+        "Here is a variation including an icon. Mind it always goes on the left.",
     },
   },
 };
@@ -139,8 +128,7 @@ Project.args = {
 Project.parameters = {
   docs: {
     description: {
-      story:
-        'Apply the className "btn-rk-green" when utilizing the button for a project entity.',
+      story: 'Use the class "btn-rk-green" when working on project components.',
     },
   },
 };
@@ -155,8 +143,7 @@ Datasets.args = {
 Datasets.parameters = {
   docs: {
     description: {
-      story:
-        'Apply the className "btn-rk-pink" when utilizing the button for a dataset entity.',
+      story: 'Use the class "btn-rk-pink" when working on dataset components.',
     },
   },
 };
@@ -173,7 +160,7 @@ Workflows.parameters = {
   docs: {
     description: {
       story:
-        'Apply the className "btn-rk-yellow" when utilizing the button for a workflow entity.',
+        'Use the class "btn-rk-yellow" when working on workflow components.',
     },
   },
 };
@@ -184,13 +171,30 @@ export const Informative = (args: LabelsProps) => (
   </>
 );
 Informative.args = {
-  text: "My Button",
+  text: "Update Version",
 };
 Informative.parameters = {
   docs: {
     description: {
       story:
-        'When employing a button for a non-entity-associated action that is not obligatory, apply the "btn-info" className.',
+        "Use `btn-info` in the context of an Info alert or to enhance user experience by offering context or supplementary actions. It is not associated with critical actions. E.G `Update Version`.",
+    },
+  },
+};
+
+export const Warning = (args: LabelsProps) => (
+  <>
+    <Button className="btn-warning">{args.text}</Button>
+  </>
+);
+Warning.args = {
+  text: "Discard Changes",
+};
+Warning.parameters = {
+  docs: {
+    description: {
+      story:
+        "Use `btn-warning` in the context of a Warning Alert, or to signal caution or potential risks. It is employed for actions that might have consequences but can still be undone or rectified.",
     },
   },
 };
@@ -201,13 +205,13 @@ export const Error = (args: LabelsProps) => (
   </>
 );
 Error.args = {
-  text: "My Button",
+  text: "Delete Project",
 };
 Error.parameters = {
   docs: {
     description: {
       story:
-        'For a button that performs a potentially risky action leading to a change in operation state, apply the "btn-danger" className',
+        "Use `btn-danger` in the context of a Danger Alert, or when suggesting a high-risk actions action that can lead to critical or irreversible outcomes. (E.G. `Delete Project` or updating a project when it is on a very outdated version not supported anymore by the UI).",
     },
   },
 };
