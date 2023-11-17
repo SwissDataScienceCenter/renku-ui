a77261c71da0c3bbfcd5b12277b73f1ba28c810d
Lorenzo <lorenzo.cavazzi.tech@gmail.com>
2023-11-17 09:18:25 +0100

    chore: restore tests

--- tests/cypress/e2e/projectSettings.spec.ts
+++ tests/cypress/e2e/projectSettings.spec.ts
@@ -18,224 +18,224 @@
 
 import fixtures from "../support/renkulab-fixtures";
 
-// // describe("Project settings page", () => {
-// //   beforeEach(() => {
-// //     fixtures.config().versions().userTest();
-// //     fixtures
-// //       .projects()
-// //       .projectTest()
-// //       .projectById()
-// //       .getProjectKG()
-// //       .projectLockStatus()
-// //       .projectMigrationUpToDate()
-// //       .sessionServersEmpty();
-// //     cy.visit("/projects/e2e/local-test-project");
-// //   });
+describe("Project settings page", () => {
+  beforeEach(() => {
+    fixtures.config().versions().userTest();
+    fixtures
+      .projects()
+      .projectTest()
+      .projectById()
+      .getProjectKG()
+      .projectLockStatus()
+      .projectMigrationUpToDate()
+      .sessionServersEmpty();
+    cy.visit("/projects/e2e/local-test-project");
+  });
 
-// //   it("update project tags", () => {
-// //     fixtures.updateProjectKG({
-// //       fixture: "project/update-project-tag-description.json",
-// //     });
-// //     fixtures.getProjectKG({
-// //       name: "getProjectKGEdited",
-// //       fixture: "project/project-kg-edited.json",
-// //     });
-// //     cy.visit("/projects/e2e/local-test-project/settings");
-// //     cy.getDataCy("keywords-input").should("not.contain.text", "abcde");
-// //     cy.getDataCy("keywords-input").type("abcde");
-// //     cy.getDataCy("projectKeywords-button").click();
-// //     cy.wait("@updateProjectKG");
-// //     cy.wait("@getProjectKGEdited");
-// //     cy.getDataCy("entity-tag-list").should("contain.text", "abcde");
-// //   });
+  it("update project tags", () => {
+    fixtures.updateProjectKG({
+      fixture: "project/update-project-tag-description.json",
+    });
+    fixtures.getProjectKG({
+      name: "getProjectKGEdited",
+      fixture: "project/project-kg-edited.json",
+    });
+    cy.visit("/projects/e2e/local-test-project/settings");
+    cy.getDataCy("keywords-input").should("not.contain.text", "abcde");
+    cy.getDataCy("keywords-input").type("abcde");
+    cy.getDataCy("projectKeywords-button").click();
+    cy.wait("@updateProjectKG");
+    cy.wait("@getProjectKGEdited");
+    cy.getDataCy("entity-tag-list").should("contain.text", "abcde");
+  });
 
-// //   it("update project visibility - success", () => {
-// //     cy.visit("/projects/e2e/local-test-project/settings");
+  it("update project visibility - success", () => {
+    cy.visit("/projects/e2e/local-test-project/settings");
 
-// //     // public visibility should be checked
-// //     cy.getDataCy("visibility-public").should("be.checked");
+    // public visibility should be checked
+    cy.getDataCy("visibility-public").should("be.checked");
 
-// //     // cancel a change shouldn't change the visibility
-// //     cy.getDataCy("visibility-private").click();
-// //     cy.get(".modal-title").should(
-// //       "contain.text",
-// //       "Change visibility to Private"
-// //     );
-// //     cy.getDataCy("cancel-visibility-btn").click();
-// //     cy.getDataCy("visibility-public").should("be.checked");
+    // cancel a change shouldn't change the visibility
+    cy.getDataCy("visibility-private").click();
+    cy.get(".modal-title").should(
+      "contain.text",
+      "Change visibility to Private"
+    );
+    cy.getDataCy("cancel-visibility-btn").click();
+    cy.getDataCy("visibility-public").should("be.checked");
 
-// //     // changing to internal will works
-// //     fixtures.updateProjectKG({
-// //       fixture: "project/visibility/visibility-change-accepted.json",
-// //     });
-// //     cy.getDataCy("visibility-internal").click();
-// //     cy.getDataCy("update-visibility-btn").click();
-// //     cy.wait("@updateProjectKG");
-// //     cy.get(".modal-body").should(
-// //       "contain.text",
-// //       "The visibility of the project has been modified"
-// //     );
-// //   });
+    // changing to internal will works
+    fixtures.updateProjectKG({
+      fixture: "project/visibility/visibility-change-accepted.json",
+    });
+    cy.getDataCy("visibility-internal").click();
+    cy.getDataCy("update-visibility-btn").click();
+    cy.wait("@updateProjectKG");
+    cy.get(".modal-body").should(
+      "contain.text",
+      "The visibility of the project has been modified"
+    );
+  });
 
-// //   it("update project visibility - failure", () => {
-// //     cy.visit("/projects/e2e/local-test-project/settings");
+  it("update project visibility - failure", () => {
+    cy.visit("/projects/e2e/local-test-project/settings");
 
-// //     // public visibility should be checked
-// //     cy.getDataCy("visibility-public").should("be.checked");
+    // public visibility should be checked
+    cy.getDataCy("visibility-public").should("be.checked");
 
-// //     // changing to internal won't work if visibility is restricted
-// //     // ? we are slightly cheating here cause we are not disabling the buttons
-// //     cy.getDataCy("visibility-internal").click();
-// //     fixtures.updateProjectKG({
-// //       fixture: "project/visibility/error-update-visibility.json",
-// //       statusCode: 400,
-// //     });
-// //     cy.getDataCy("update-visibility-btn").click();
-// //     cy.wait("@updateProjectKG");
-// //     cy.get(".modal-body").should(
-// //       "contain.text",
-// //       "Internal is not allowed in a private group."
-// //     );
-// //     cy.get(".modal-content .btn-close").click();
-// //   });
+    // changing to internal won't work if visibility is restricted
+    // ? we are slightly cheating here cause we are not disabling the buttons
+    cy.getDataCy("visibility-internal").click();
+    fixtures.updateProjectKG({
+      fixture: "project/visibility/error-update-visibility.json",
+      statusCode: 400,
+    });
+    cy.getDataCy("update-visibility-btn").click();
+    cy.wait("@updateProjectKG");
+    cy.get(".modal-body").should(
+      "contain.text",
+      "Internal is not allowed in a private group."
+    );
+    cy.get(".modal-content .btn-close").click();
+  });
 
-// //   it("update description", () => {
-// //     cy.visit("/projects/e2e/local-test-project/settings");
+  it("update description", () => {
+    cy.visit("/projects/e2e/local-test-project/settings");
 
-// //     // check there is no description
-// //     cy.getDataCy("description-input").should(
-// //       "not.have.value",
-// //       "description abcde"
-// //     );
-// //     cy.getDataCy("entity-description").should(
-// //       "not.contain.text",
-// //       "description abcde"
-// //     );
-// //     cy.getDataCy("entity-description").should(
-// //       "contain.text",
-// //       "This project has no description"
-// //     );
+    // check there is no description
+    cy.getDataCy("description-input").should(
+      "not.have.value",
+      "description abcde"
+    );
+    cy.getDataCy("entity-description").should(
+      "not.contain.text",
+      "description abcde"
+    );
+    cy.getDataCy("entity-description").should(
+      "contain.text",
+      "This project has no description"
+    );
 
-// //     // edit the description -- load the new fixtures
-// //     cy.getDataCy("description-input").type("description abcde");
-// //     fixtures.updateProjectKG({ fixture: "project/project-kg-edited.json" });
-// //     fixtures.getProjectKG({
-// //       name: "getProjectKGdescription",
-// //       fixture: "project/edit/project-kg-description.json",
-// //     });
-// //     cy.getDataCy("projectDescription-button").click();
+    // edit the description -- load the new fixtures
+    cy.getDataCy("description-input").type("description abcde");
+    fixtures.updateProjectKG({ fixture: "project/project-kg-edited.json" });
+    fixtures.getProjectKG({
+      name: "getProjectKGdescription",
+      fixture: "project/edit/project-kg-description.json",
+    });
+    cy.getDataCy("projectDescription-button").click();
 
-// //     // verify the change goes through but is not immediate everywhere else (E.G. on the header)
-// //     cy.getDataCy("settings-description")
-// //       .get(".success-feedback")
-// //       .contains("Updated");
-// //     cy.getDataCy("description-input").should(
-// //       "not.contain.text",
-// //       "description abcde"
-// //     );
+    // verify the change goes through but is not immediate everywhere else (E.G. on the header)
+    cy.getDataCy("settings-description")
+      .get(".success-feedback")
+      .contains("Updated");
+    cy.getDataCy("description-input").should(
+      "not.contain.text",
+      "description abcde"
+    );
 
-// //     // Check the header also reflects the change onnce KG processes the metadata
-// //     fixtures.getKgStatus();
-// //     cy.wait("@getProjectKGdescription");
-// //     cy.getDataCy("entity-description").should("contain", "description abcde");
-// //     cy.getDataCy("description-input").should("have.value", "description abcde");
-// //   });
+    // Check the header also reflects the change onnce KG processes the metadata
+    fixtures.getKgStatus();
+    cy.wait("@getProjectKGdescription");
+    cy.getDataCy("entity-description").should("contain", "description abcde");
+    cy.getDataCy("description-input").should("have.value", "description abcde");
+  });
 
-// //   it("displays project settings sessions", () => {
-// //     fixtures.sessionServerOptions().resourcePoolsTest().projectConfigShow();
-// //     cy.visit("/projects/e2e/local-test-project/settings/sessions");
-// //     cy.wait("@getSessionServerOptions");
-// //     cy.contains("Number of CPUs").should("be.visible");
-// //     cy.contains("Amount of Memory").should("be.visible");
-// //     cy.contains("Amount of Storage").should("be.visible");
-// //     cy.contains("Number of GPUs").should("be.visible");
-// //   });
+  it("displays project settings sessions", () => {
+    fixtures.sessionServerOptions().resourcePoolsTest().projectConfigShow();
+    cy.visit("/projects/e2e/local-test-project/settings/sessions");
+    cy.wait("@getSessionServerOptions");
+    cy.contains("Number of CPUs").should("be.visible");
+    cy.contains("Amount of Memory").should("be.visible");
+    cy.contains("Amount of Storage").should("be.visible");
+    cy.contains("Number of GPUs").should("be.visible");
+  });
 
-// //   it("displays project settings with cloud-storage enabled ", () => {
-// //     fixtures
-// //       .sessionServerOptions({ cloudStorage: true })
-// //       .resourcePoolsTest()
-// //       .projectConfigShow();
-// //     cy.visit("/projects/e2e/local-test-project/settings/sessions");
-// //     cy.wait("@getSessionServerOptions");
-// //     cy.contains("Number of CPUs").should("be.visible");
-// //     cy.contains("Amount of Memory").should("be.visible");
-// //     cy.contains("Amount of Storage").should("be.visible");
-// //     cy.contains("Number of GPUs").should("be.visible");
-// //   });
+  it("displays project settings with cloud-storage enabled ", () => {
+    fixtures
+      .sessionServerOptions({ cloudStorage: true })
+      .resourcePoolsTest()
+      .projectConfigShow();
+    cy.visit("/projects/e2e/local-test-project/settings/sessions");
+    cy.wait("@getSessionServerOptions");
+    cy.contains("Number of CPUs").should("be.visible");
+    cy.contains("Amount of Memory").should("be.visible");
+    cy.contains("Amount of Storage").should("be.visible");
+    cy.contains("Number of GPUs").should("be.visible");
+  });
 
-// //   it("displays project settings complete", () => {
-// //     fixtures.sessionServerOptions().resourcePoolsTest().projectConfigShow();
-// //     cy.visit("/projects/e2e/local-test-project/settings/sessions");
-// //     cy.wait("@getSessionServerOptions");
-// //     cy.wait("@getProjectConfigShow");
-// //     cy.contains("Number of CPUs").should("be.visible");
-// //     cy.contains("Amount of Memory").should("be.visible");
-// //     cy.contains("Amount of Storage").should("be.visible");
-// //     cy.contains("Number of GPUs").should("be.visible");
-// //     cy.get("button.active").contains("/lab").should("be.visible");
-// //   });
+  it("displays project settings complete", () => {
+    fixtures.sessionServerOptions().resourcePoolsTest().projectConfigShow();
+    cy.visit("/projects/e2e/local-test-project/settings/sessions");
+    cy.wait("@getSessionServerOptions");
+    cy.wait("@getProjectConfigShow");
+    cy.contains("Number of CPUs").should("be.visible");
+    cy.contains("Amount of Memory").should("be.visible");
+    cy.contains("Amount of Storage").should("be.visible");
+    cy.contains("Number of GPUs").should("be.visible");
+    cy.get("button.active").contains("/lab").should("be.visible");
+  });
 
-// //   it("displays project settings error", () => {
-// //     fixtures.sessionServerOptions().projectConfigShow({ error: true });
-// //     cy.visit("/projects/e2e/local-test-project/settings/sessions");
-// //     cy.wait("@getSessionServerOptions");
-// //     cy.wait("@getProjectLockStatus");
-// //     cy.wait("@getProjectConfigShow");
-// //     cy.contains("Number of CPUs").should("not.exist");
-// //     cy.contains("Error").should("be.visible");
-// //   });
+  it("displays project settings error", () => {
+    fixtures.sessionServerOptions().projectConfigShow({ error: true });
+    cy.visit("/projects/e2e/local-test-project/settings/sessions");
+    cy.wait("@getSessionServerOptions");
+    cy.wait("@getProjectLockStatus");
+    cy.wait("@getProjectConfigShow");
+    cy.contains("Number of CPUs").should("not.exist");
+    cy.contains("Error").should("be.visible");
+  });
 
-// //   it("displays project settings legacy error", () => {
-// //     fixtures.sessionServerOptions().projectConfigShow({ legacyError: true });
-// //     cy.visit("/projects/e2e/local-test-project/settings/sessions");
-// //     cy.wait("@getSessionServerOptions");
-// //     cy.wait("@getProjectLockStatus");
-// //     cy.wait("@getProjectConfigShow");
-// //     cy.contains("Number of CPUs").should("not.exist");
-// //     cy.contains("Error").should("be.visible");
-// //     cy.contains("[Show details]").should("be.visible");
-// //   });
+  it("displays project settings legacy error", () => {
+    fixtures.sessionServerOptions().projectConfigShow({ legacyError: true });
+    cy.visit("/projects/e2e/local-test-project/settings/sessions");
+    cy.wait("@getSessionServerOptions");
+    cy.wait("@getProjectLockStatus");
+    cy.wait("@getProjectConfigShow");
+    cy.contains("Number of CPUs").should("not.exist");
+    cy.contains("Error").should("be.visible");
+    cy.contains("[Show details]").should("be.visible");
+  });
 
-// //   it("delete a project", () => {
-// //     fixtures.deleteProject();
-// //     cy.visit("/projects/e2e/local-test-project/settings");
-// //     cy.wait("@getProjectKG");
-// //     cy.wait("@getProjectLockStatus");
-// //     cy.wait("@getMigration");
+  it("delete a project", () => {
+    fixtures.deleteProject();
+    cy.visit("/projects/e2e/local-test-project/settings");
+    cy.wait("@getProjectKG");
+    cy.wait("@getProjectLockStatus");
+    cy.wait("@getMigration");
 
-// //     cy.getDataCy("project-settings-general-delete-project")
-// //       .should("be.visible")
-// //       .find("button")
-// //       .contains("Delete project")
-// //       .should("be.visible")
-// //       .as("delete-button")
-// //       .scrollIntoView();
-// //     cy.get("@delete-button").click();
+    cy.getDataCy("project-settings-general-delete-project")
+      .should("be.visible")
+      .find("button")
+      .contains("Delete project")
+      .should("be.visible")
+      .as("delete-button")
+      .scrollIntoView();
+    cy.get("@delete-button").click();
 
-// //     cy.contains("Are you absolutely sure?");
-// //     cy.get("button").contains("Yes, delete this project").should("be.disabled");
-// //     cy.get("input[name=project-settings-general-delete-confirm-box]").type(
-// //       "e2e/local-test-project"
-// //     );
-// //     cy.get("button")
-// //       .contains("Yes, delete this project")
-// //       .should("be.enabled")
-// //       .click();
-// //     cy.wait("@deleteProject");
+    cy.contains("Are you absolutely sure?");
+    cy.get("button").contains("Yes, delete this project").should("be.disabled");
+    cy.get("input[name=project-settings-general-delete-confirm-box]").type(
+      "e2e/local-test-project"
+    );
+    cy.get("button")
+      .contains("Yes, delete this project")
+      .should("be.enabled")
+      .click();
+    cy.wait("@deleteProject");
 
-// //     cy.url().should("not.contain", "/projects");
-// //     cy.get(".Toastify")
-// //       .contains("Project e2e/local-test-project deleted")
-// //       .should("be.visible");
-// //   });
+    cy.url().should("not.contain", "/projects");
+    cy.get(".Toastify")
+      .contains("Project e2e/local-test-project deleted")
+      .should("be.visible");
+  });
 
-// //   it("delete a project - not allowed", () => {
-// //     fixtures.userNone();
-// //     cy.visit("/projects/e2e/local-test-project/settings");
-// //     cy.getDataCy("project-settings-general-delete-project").should("not.exist");
-// //   });
-// // });
+  it("delete a project - not allowed", () => {
+    fixtures.userNone();
+    cy.visit("/projects/e2e/local-test-project/settings");
+    cy.getDataCy("project-settings-general-delete-project").should("not.exist");
+  });
+});
 
 describe("Cloud storage settings page", () => {
   beforeEach(() => {
