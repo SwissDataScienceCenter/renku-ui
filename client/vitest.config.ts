d5d433d931198e2e474c711b0b07c59c73f67c7f
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2023-11-28 15:06:26 +0100

    more fixes

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    // singleThread: true,
+    singleThread: true,
     setupFiles: "./testSetup.ts",
   },
 });
