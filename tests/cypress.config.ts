2439e6f260189adb3f8e214e43a3f1c0017b7fb1
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2023-11-29 13:21:58 +0100

    fix tests

--- tests/cypress.config.ts
+++ tests/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:4173",
+    baseUrl: "http://localhost:5173",
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
   },
   env: {
