e98dc90a1a10f4707c809b3fd69c95bb5fcb2f6f
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2023-12-08 09:10:04 +0100

    enable source maps

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,10 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: { outDir: "build" },
+  build: {
+    outDir: "build",
+    sourcemap: true,
+  },
   plugins: [react({ include: "/index.html" })],
   resolve: {
     alias: {
