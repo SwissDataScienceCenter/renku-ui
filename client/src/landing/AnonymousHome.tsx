0ea89f24831a58e96078690b9eab869bb985e756
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2024-02-08 13:04:04 +0100

    fix(client): temporarily fix search bar styles (#3019)

--- client/src/landing/AnonymousHome.tsx
+++ client/src/landing/AnonymousHome.tsx
@@ -51,6 +51,12 @@ import WhoWeAre from "./WhoWeAre/WhoWeAre";
 import type { AnonymousHomeConfig } from "./anonymousHome.types";
 import { BottomNav, TopNav } from "./anonymousHomeNav";
 
+// ? react-autosuggest styles are defined there q_q
+// ? also, the order of import matters here q_q
+import "../project/new/Project.style.css";
+// ? the "quick-nav" class is used in this file
+import "../components/quicknav/QuickNav.style.css";
+
 export default function AnonymousHome() {
   const { client, model, params } = useContext(AppContext);
 
