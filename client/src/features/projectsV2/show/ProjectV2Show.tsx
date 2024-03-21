824f2b7711ff080721c6a8b77c124cc316753456
Chandrasekhar Ramakrishnan <ciyer@users.noreply.github.com>
2024-03-21 13:45:18 +0100

    improving display of namespace on project

--- client/src/features/projectsV2/show/ProjectV2Show.tsx
+++ client/src/features/projectsV2/show/ProjectV2Show.tsx
@@ -59,8 +59,12 @@ function ProjectV2Header({
   const projectListUrl = Url.get(Url.pages.v2Projects.list);
   return (
     <>
-      <div>
-        {project.namespace}/{project.slug}
+      <div className="fw-medium">
+        <div className="mb-0">{project.namespace}/</div>
+        <div className="ms-2">
+          {"  "}
+          {project.slug}
+        </div>
       </div>
       <div className="fst-italic">{project.visibility}</div>
       <TimeCaption datetime={project.creation_date} prefix="Created" />{" "}
