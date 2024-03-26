b32e39bd9083bdd1f8b5ca9276d7e835d5567340
Chandrasekhar Ramakrishnan <ciyer@users.noreply.github.com>
2024-03-26 10:57:25 +0100

    fix to match changed API for choosing react-select option

--- tests/cypress/e2e/projectV2.spec.ts
+++ tests/cypress/e2e/projectV2.spec.ts
@@ -69,11 +69,7 @@ describe("Add new v2 project", () => {
     cy.contains("New Project (V2)").should("be.visible");
     cy.getDataCy("project-name-input").clear().type(newProjectTitle);
     cy.getDataCy("project-slug-input").should("have.value", slug);
-    cy.findReactSelectOptions(
-      "@listV2Namespace",
-      "project-namespace-input",
-      "namespace-select"
-    )
+    cy.findReactSelectOptions("project-namespace-input", "namespace-select")
       .first()
       .click();
     cy.contains("user1-uuid").should("exist");
