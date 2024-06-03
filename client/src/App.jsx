bb50549d3f844627b6f6fd80c0804082f5a5bec4
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2024-06-03 16:17:52 +0200

    app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -92,9 +92,6 @@ function CentralContentContainer(props) {
     webSocket: socket,
   };
 
-  // check anonymous sessions settings
-  const blockAnonymous = !user.logged && !props.params["ANONYMOUS_SESSIONS"];
-
   return (
     <div className="d-flex flex-grow-1">
       <AppContext.Provider value={appContext}>
@@ -150,15 +147,7 @@ function CentralContentContainer(props) {
             </ContainerWrap>
           </CompatRoute>
           <CompatRoute Route path="/projects/:subUrl+">
-            <LazyProjectView
-              client={props.client}
-              params={props.params}
-              model={props.model}
-              user={props.user}
-              blockAnonymous={blockAnonymous}
-              notifications={notifications}
-              socket={socket}
-            />
+            <LazyProjectView />
           </CompatRoute>
           <Route exact path={Url.get(Url.pages.sessions)}>
             {!user.logged ? <LazyAnonymousSessionsList /> : <Redirect to="/" />}
