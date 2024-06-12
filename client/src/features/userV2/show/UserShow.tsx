939b5888a68fa006cf9fab07080641f5431d23a7
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2024-06-12 15:40:01 +0200

    badges

--- client/src/features/userV2/show/UserShow.tsx
+++ client/src/features/userV2/show/UserShow.tsx
@@ -17,12 +17,14 @@
  */
 
 import { skipToken } from "@reduxjs/toolkit/query";
+import cx from "classnames";
 import { useEffect } from "react";
 import {
   generatePath,
   useNavigate,
   useParams,
 } from "react-router-dom-v5-compat";
+import { Badge } from "reactstrap";
 
 import { Loader } from "../../../components/Loader";
 import ContainerWrap from "../../../components/container/ContainerWrap";
@@ -31,6 +33,7 @@ import { ABSOLUTE_ROUTES } from "../../../routing/routes.constants";
 import { useGetNamespacesByGroupSlugQuery } from "../../projectsV2/api/projectV2.enhanced-api";
 import ProjectV2ListDisplay from "../../projectsV2/list/ProjectV2ListDisplay";
 import UserNotFound from "../../projectsV2/notFound/UserNotFound";
+import { useGetUserQuery } from "../../user/dataServicesUser.api";
 
 export default function UserShow() {
   const { username } = useParams<{ username: string }>();
@@ -69,7 +72,11 @@ export default function UserShow() {
 
   return (
     <ContainerWrap>
-      <h1>{name ?? "Unknown user"}</h1>
+      <div className={cx("d-flex", "align-items-start", "h1")}>
+        <h1 className={cx("mb-0")}>{name ?? "Unknown user"}</h1>
+        <UserBadge />
+        <ItsYouBadge username={username} />
+      </div>
       <p className="fs-4">{`@${username}`}</p>
 
       <section>
@@ -78,4 +85,48 @@ export default function UserShow() {
       </section>
     </ContainerWrap>
   );
+}
+
+function UserBadge() {
+  return (
+    <Badge
+      className={cx(
+        "ms-4",
+        "border",
+        "border-success",
+        "bg-success-subtle",
+        "text-success"
+      )}
+      pill
+    >
+      User
+    </Badge>
+  );
+}
+
+interface ItsYouBadgeProps {
+  username: string;
+}
+
+function ItsYouBadge({ username }: ItsYouBadgeProps) {
+  const { data: currentUser } = useGetUserQuery();
+
+  if (currentUser?.username === username) {
+    return (
+      <Badge
+        className={cx(
+          "ms-2",
+          "border",
+          "border-warning",
+          "bg-warning-subtle",
+          "text-warning"
+        )}
+        pill
+      >
+        It's you!
+      </Badge>
+    );
+  }
+
+  return null;
 }
