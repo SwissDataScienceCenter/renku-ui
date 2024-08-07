452a935d991450efb668b6593ea13a1fe908a6aa
Lorenzo <lorenzo.cavazzi.tech@gmail.com>
2024-08-07 17:56:45 +0200

    temp

--- client/src/features/dashboardV2/HelpV2.tsx
+++ client/src/features/dashboardV2/HelpV2.tsx
@@ -15,169 +15,74 @@
  * See the License for the specific language governing permissions and
  * limitations under the License
  */
-import cx from "classnames";
-import { ChatSquareDots, Github, JournalText } from "react-bootstrap-icons";
-import { useContext } from "react";
-import { Route, Routes } from "react-router-dom-v5-compat";
-import { Card, CardBody, CardHeader, Col, Nav, NavItem, Row } from "reactstrap";
 
-import {
-  ExternalDocsLink,
-  ExternalIconLink,
-} from "../../components/ExternalLinks";
-import RenkuNavLinkV2 from "../../components/RenkuNavLinkV2";
-import HelpRelease from "../../help/HelpRelease";
-import PrivacyPolicy from "../../help/PrivacyPolicy";
-import TermsOfService from "../../help/TermsOfService";
-import { isStatusConfigured } from "../../statuspage";
-import { Links } from "../../utils/constants/Docs";
-import AppContext from "../../utils/context/appContext";
-import { DEFAULT_APP_PARAMS } from "../../utils/context/appParams.constants";
-import StatusSummary from "../platform/components/StatusSummary";
+import { useState } from "react";
 
-type HelpNavProps = {
-  statuspageId: string;
-};
-function HelpNav({ statuspageId }: HelpNavProps) {
-  const { params } = useContext(AppContext);
-  if (params == null) return null;
-  const privacyPolicyConfigured = params.TERMS_PAGES_ENABLED;
-  const termsConfigured = privacyPolicyConfigured;
+function Parent() {
+  const fieldName = "Family name";
+
+  const [inputValue, setInputValue] = useState("");
+  const handleInputChange = (text: string) => setInputValue(text);
+
+  console.log("Parent render"); // eslint-disable-line
+
   return (
-    <Nav tabs className="mb-3">
-      <NavItem>
-        <RenkuNavLinkV2 end to=".">
-          Getting Help
-        </RenkuNavLinkV2>
-      </NavItem>
-      {isStatusConfigured(statuspageId) && (
-        <NavItem>
-          <RenkuNavLinkV2 to="status">Status</RenkuNavLinkV2>
-        </NavItem>
-      )}
-      <NavItem>
-        <RenkuNavLinkV2 to="release">Release Information</RenkuNavLinkV2>
-      </NavItem>
-      {termsConfigured && (
-        <NavItem>
-          <RenkuNavLinkV2 to="tos">Terms of Use</RenkuNavLinkV2>
-        </NavItem>
-      )}
-      {privacyPolicyConfigured && (
-        <NavItem>
-          <RenkuNavLinkV2 to="privacy">Privacy Policy</RenkuNavLinkV2>
-        </NavItem>
-      )}
-    </Nav>
-  );
-}
+    <div className="p-4 bg-info-subtle d-flex gap-4">
+      <ChildLeft
+        fieldName={fieldName}
+        inputValue={inputValue}
+        handleInputChange={handleInputChange}
+      />
 
-function HelpGetting() {
-  return (
-    <div>
-      <p>
-        There are several channels available for getting help with RenkuLab.
-        Depending on your needs, one or another may be better for you.
-      </p>
-      <Row className="g-3">
-        <Col xs={12} md={6}>
-          <Card>
-            <CardHeader>
-              <h4 className="mb-0">
-                <ExternalIconLink
-                  url={Links.DISCOURSE}
-                  icon={<JournalText className={cx("bi", "me-1")} />}
-                  text="Forum"
-                />
-              </h4>
-            </CardHeader>
-            <CardBody>
-              <p className="mb-0">
-                We maintain a{" "}
-                <ExternalDocsLink url={Links.DISCOURSE} title="help forum" />{" "}
-                for discussion about Renku. This is a good place to ask
-                questions and find answers.
-              </p>
-            </CardBody>
-          </Card>
-        </Col>
-
-        <Col xs={12} md={6}>
-          <Card>
-            <CardHeader>
-              <h4 className="mb-0">
-                <ExternalIconLink
-                  url={Links.GITTER}
-                  icon={<ChatSquareDots className={cx("bi", "me-1")} />}
-                  text="Gitter"
-                />
-              </h4>
-            </CardHeader>
-            <CardBody>
-              <p className="mb-0">
-                Want to reach out to the development team live? Contact us on{" "}
-                <ExternalDocsLink url={Links.GITTER} title="Gitter" />, we would
-                be happy to chat with you.
-              </p>
-            </CardBody>
-          </Card>
-        </Col>
-        <Col xs={12} md={6}>
-          <Card>
-            <CardHeader>
-              <h4 className="mb-0">
-                <ExternalIconLink
-                  url={Links.GITHUB}
-                  icon={<Github className={cx("bi", "me-1")} />}
-                  text="GitHub"
-                />
-              </h4>
-            </CardHeader>
-            <CardBody>
-              <p className="mb-0">
-                Renku is open source and being developed on{" "}
-                <ExternalDocsLink url={Links.GITHUB} title="GitHub" />. This is
-                the best place to report issues and ask for new features, but
-                feel free to contact us with questions, comments, or any kind of
-                feedback.
-              </p>
-            </CardBody>
-          </Card>
-        </Col>
-      </Row>
+      <ChildRight fieldName={fieldName} />
     </div>
   );
 }
 
-function HelpContent() {
+function ChildLeft({
+  fieldName,
+  inputValue,
+  handleInputChange,
+}: {
+  fieldName: string;
+  inputValue: string;
+  handleInputChange: (text: string) => void;
+}) {
   return (
-    <Routes>
-      <Route path="/" element={<HelpGetting />} />
-      <Route path="contact" element={<HelpGetting />} />
-      <Route path="status" element={<StatusSummary />} />
-      <Route path="release" element={<HelpRelease />} />
-      <Route path="tos" element={<TermsOfService />} />
-      <Route path="privacy" element={<PrivacyPolicy />} />
-    </Routes>
+    <div className="bg-dark bg-opacity-10 p-4 w-50">
+      <label>{fieldName}</label>
+      <input
+        className="w-100"
+        type="text"
+        value={inputValue}
+        onChange={(e) => handleInputChange(e.target.value)}
+      />
+    </div>
+  );
+}
+
+function ChildRight({ fieldName }: { fieldName: string }) {
+  const [inputValue, setInputValue] = useState("");
+  const handleInputChange = (text: string) => setInputValue(text);
+
+  return (
+    <div className="bg-dark bg-opacity-10 p-4 w-50">
+      <label>{fieldName}</label>
+      <input
+        className="w-100"
+        type="text"
+        value={inputValue}
+        onChange={(e) => handleInputChange(e.target.value)}
+      />
+    </div>
   );
 }
 
 export default function Help() {
-  const { params } = useContext(AppContext);
-  const statuspageId =
-    params?.STATUSPAGE_ID ?? DEFAULT_APP_PARAMS.STATUSPAGE_ID;
-
   return (
-    <Row>
-      <Col xs={12}>
-        <h2>Help</h2>
-      </Col>
-      <Col xs={12}>
-        <HelpNav statuspageId={statuspageId} />
-      </Col>
-      <Col xs={12}>
-        <HelpContent />
-      </Col>
-    </Row>
+    <div className="container">
+      <h2>React components</h2>
+      <Parent />
+    </div>
   );
 }
