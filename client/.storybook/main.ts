5cd5809fef27c71057a89117f991c9820717af5b
Lorenzo <lorenzo.cavazzi.tech@gmail.com>
2024-09-12 14:37:34 +0200

    build: update storybook 7 and streamline the configuration

--- client/.storybook/main.ts
+++ client/.storybook/main.ts
@@ -1,11 +1,9 @@
 import type { StorybookConfig } from "@storybook/react-vite";
-import { mergeConfig } from "vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
   staticDirs: ["../public"],
   addons: [
-    "@storybook/addon-docs",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-links",
@@ -16,7 +14,6 @@ const config: StorybookConfig = {
     name: "@storybook/react-vite",
     options: {},
   },
-  core: { builder: "@storybook/builder-vite" },
   typescript: {
     reactDocgen: "react-docgen-typescript",
     reactDocgenTypescriptOptions: {
