0e336527cabdca0eb0f502b48daab8b8620c5d5b
Chandrasekhar Ramakrishnan <ciyer@users.noreply.github.com>
2024-09-26 08:12:35 +0200

    Revert "fixed hook dependencies" -- it caused problems for testing the connection
    This reverts commit e876667a16af6ef150c0499f9a75f949402f845e.

--- client/src/features/dataConnectorsV2/components/DataConnectorModal/index.tsx
+++ client/src/features/dataConnectorsV2/components/DataConnectorModal/index.tsx
@@ -300,7 +300,7 @@ export default function DataConnectorModal({
     };
 
     return cleanup;
-  }, [reset]);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   const schemaRequiresProvider = useMemo(
     () => hasProviderShortlist(flatDataConnector.schema),
