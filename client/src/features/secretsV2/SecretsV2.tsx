6bd237cb3ba48181b314c71f2e8e0bb08ec8bce6
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2024-11-20 10:16:34 +0100

    wip

--- client/src/features/secretsV2/SecretsV2.tsx
+++ client/src/features/secretsV2/SecretsV2.tsx
@@ -16,6 +16,195 @@
  * limitations under the License.
  */
 
+import cx from "classnames";
+import {
+  Badge,
+  Card,
+  CardBody,
+  CardHeader,
+  Col,
+  ListGroup,
+  Row,
+} from "reactstrap";
+
+import { RtkOrNotebooksError } from "../../components/errors/RtkErrorAlert";
+import { Loader } from "../../components/Loader";
+import LoginAlert from "../../components/loginAlert/LoginAlert";
+import type { User } from "../../model/renkuModels.types";
+import useLegacySelector from "../../utils/customHooks/useLegacySelector.hook";
+import {
+  type SecretWithId,
+  useGetUserSecretsQuery,
+} from "../usersV2/api/users.api";
+import { Database, QuestionSquare, ShieldLock } from "react-bootstrap-icons";
+import { useMemo } from "react";
+
 export default function SecretsV2() {
-  return <div>{"<SecretsV2>"}</div>;
+  const user = useLegacySelector<User>((state) => state.stateModel.user);
+
+  if (!user.fetched) return <Loader />;
+
+  return (
+    <>
+      <Row>
+        <Col>
+          <h2>User Secrets</h2>
+          <SecretsPageInfo />
+        </Col>
+      </Row>
+      {user.logged && (
+        <div className={cx("d-flex", "flex-column", "gap-4")}>
+          <SessionSecrets />
+          <DataConnectorSecrets />
+          <UnusedSecrets />
+        </div>
+      )}
+    </>
+  );
+}
+
+function SecretsPageInfo() {
+  const userLogged = useLegacySelector<User["logged"]>(
+    (state) => state.stateModel.user.logged
+  );
+
+  if (!userLogged) {
+    return (
+      <LoginAlert
+        logged={false}
+        textIntro="Only authenticated users can create and manage secrets."
+        textPost="to access this page."
+      />
+    );
+  }
+
+  return <p>Here you can store secrets to use in your sessions.</p>;
+}
+
+function SessionSecrets() {
+  const {
+    data: secrets,
+    isLoading,
+    error,
+  } = useGetUserSecretsQuery({ userSecretsParams: { kind: "general" } });
+
+  const secretsUsedInSessions = useMemo(
+    () =>
+      secrets?.filter(
+        ({ session_secret_ids }) => session_secret_ids.length > 0
+      ),
+    [secrets]
+  );
+
+  const content = isLoading ? (
+    <p>
+      <Loader className="me-1" inline size={16} />
+      Loading secrets...
+    </p>
+  ) : error || !secretsUsedInSessions ? (
+    <>
+      <p>Error: could not load user secrets.</p>
+      {error && <RtkOrNotebooksError error={error} dismissible={false} />}
+    </>
+  ) : (
+    <SessionSecretsContent secretsUsedInSessions={secretsUsedInSessions} />
+  );
+
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <div className={cx("align-items-center", "d-flex")}>
+            <h4 className={cx("m-0", "me-2")}>
+              <ShieldLock className={cx("me-1", "bi")} />
+              Session Secrets
+            </h4>
+            {secretsUsedInSessions && (
+              <Badge>{secretsUsedInSessions.length}</Badge>
+            )}
+          </div>
+        </CardHeader>
+        <CardBody className={cx(secretsUsedInSessions?.length == 0 && "pb-0")}>
+          {content}
+        </CardBody>
+      </Card>
+    </>
+  );
+}
+
+interface SessionSecretsContentProps {
+  secretsUsedInSessions: SecretWithId[];
+}
+
+function SessionSecretsContent({
+  secretsUsedInSessions,
+}: SessionSecretsContentProps) {
+  if (!secretsUsedInSessions.length) {
+    return (
+      <p className="fst-italic">
+        You do not have secrets ready to be used in sessions at the moment.
+      </p>
+    );
+  }
+
+  return (
+    <ListGroup flush>
+      {/* {sessionSecretSlotsWithSecrets.map((secretSlot) => (
+            <SessionSecretSlotItem
+              key={secretSlot.secretSlot.id}
+              secretSlot={secretSlot}
+            />
+          ))} */}
+    </ListGroup>
+  );
+}
+
+function DataConnectorSecrets() {
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <div className={cx("align-items-center", "d-flex")}>
+            <h4 className={cx("m-0", "me-2")}>
+              <Database className={cx("me-1", "bi")} />
+              Data Connector Secrets
+            </h4>
+            {/* {secretsUsedInSessions && (
+                  <Badge>{secretsUsedInSessions.length}</Badge>
+                )} */}
+          </div>
+        </CardHeader>
+        <CardBody
+        //  className={cx(secretsUsedInSessions?.length == 0 && "pb-0")}
+        >
+          TODO: Data Connector Secrets
+        </CardBody>
+      </Card>
+    </>
+  );
+}
+
+function UnusedSecrets() {
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <div className={cx("align-items-center", "d-flex")}>
+            <h4 className={cx("m-0", "me-2")}>
+              <QuestionSquare className={cx("me-1", "bi")} />
+              Unused Secrets
+            </h4>
+            {/* {secretsUsedInSessions && (
+                    <Badge>{secretsUsedInSessions.length}</Badge>
+                  )} */}
+          </div>
+        </CardHeader>
+        <CardBody
+        //  className={cx(secretsUsedInSessions?.length == 0 && "pb-0")}
+        >
+          TODO: Unused Secrets
+        </CardBody>
+      </Card>
+    </>
+  );
 }
