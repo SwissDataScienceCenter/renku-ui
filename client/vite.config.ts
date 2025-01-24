d192f6af3d8488094375534ddeafed84776a5420
Tasko Olevski <olevski90@gmail.com>
2025-01-24 17:40:49 +0100

    chore: run ui client locally against a deployment
    This allows the ui client to run locally and target any other Renku
    deployment as long as the gateway cookie settings on the target
    deployment are loose enough. But we can make it so that on CI
    deployments this is the case.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,36 @@
 import eslintPlugin from "@nabla/vite-plugin-eslint";
+import express from "express";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
-import { defineConfig } from "vite";
+import { defineConfig, ViteDevServer } from "vite";
+
+const app = express();
+
+app.get("/config.json", (req, res) => {
+  const baseURL = new URL(process.env.RENKU_BASE_URL);
+  const gatewayURL = new URL("/api", baseURL);
+  const uiServerURL = new URL("/ui-server", baseURL);
+  res
+    .json({
+      GATEWAY_URL: gatewayURL.href,
+      UISERVER_URL: uiServerURL.href,
+      BASE_URL: baseURL.href,
+      KEYCLOAK_REALM: "Renku",
+    })
+    .end();
+});
+
+function expressPlugin() {
+  return {
+    name: "express-plugin",
+    configureServer(server: ViteDevServer) {
+      if (process.env.RENKU_BASE_URL == null) {
+        return;
+      }
+      server.middlewares.use(app);
+    },
+  };
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +38,7 @@ export default defineConfig({
     outDir: "build",
     sourcemap: true,
   },
-  plugins: [react({ include: "/index.html" }), eslintPlugin()],
+  plugins: [react({ include: "/index.html" }), eslintPlugin(), expressPlugin()],
   resolve: {
     alias: {
       "~bootstrap": resolve(__dirname, "node_modules/bootstrap"),
