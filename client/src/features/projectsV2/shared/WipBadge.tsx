5f7fbb6b5512fc3c60ed04126863e674fe4af2ac
Chandrasekhar Ramakrishnan <ciyer@users.noreply.github.com>
2025-02-06 15:14:52 +0100

    minor: remove WIP badge from V1 secrets (#3526) (#3521)

--- client/src/features/projectsV2/shared/WipBadge.tsx
+++ client/src/features/projectsV2/shared/WipBadge.tsx
@@ -29,7 +29,7 @@ interface WipBadeProps {
 
 export default function WipBadge({
   className,
-  children = "2 .0 Early access",
+  children = "Work in progress",
   tooltip = "The platform is stable, but some core features are still being developed and will be added soon.",
 }: WipBadeProps) {
   const ref = useRef<HTMLDivElement>(null);
