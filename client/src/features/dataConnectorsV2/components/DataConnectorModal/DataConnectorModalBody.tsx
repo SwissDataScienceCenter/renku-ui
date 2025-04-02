b7d2c32b209e8f60b30f33fa420c07c6ff0a7b5d
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2025-04-02 15:56:40 +0200

    fix duplicates

--- client/src/features/dataConnectorsV2/components/DataConnectorModal/DataConnectorModalBody.tsx
+++ client/src/features/dataConnectorsV2/components/DataConnectorModal/DataConnectorModalBody.tsx
@@ -183,7 +183,7 @@ type DataConnectorMountFormFields =
   | "mountPoint"
   | "readOnly"
   | "saveCredentials";
-export function DataConnectorMount(_: AddOrEditDataConnectorProps) {
+export function DataConnectorMount() {
   const dispatch = useAppDispatch();
   const { cloudStorageState, flatDataConnector, schemata } = useAppSelector(
     (state) => state.dataConnectorFormSlice
