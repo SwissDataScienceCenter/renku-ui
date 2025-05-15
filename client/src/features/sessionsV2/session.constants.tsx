00ee90e9a3af3a3404489b52c611c14b209a6ff3
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2025-05-15 09:54:16 +0200

    fix: make custom launch work (#3690)
    Closes #3689.

--- client/src/features/sessionsV2/session.constants.tsx
+++ client/src/features/sessionsV2/session.constants.tsx
@@ -72,6 +72,10 @@ export const FAVICON_BY_SESSION_STATUS = {
   },
 };
 
+export const CUSTOM_LAUNCH_SEARCH_PARAM = "renku_custom_launch";
+
+export const ENV_VARIABLES_RESERVED_PREFIX = "RENKU";
+
 export const ENVIRONMENT_VALUES_DESCRIPTION = {
   urlPath: `Specify a subpath for your Renku session. By default, the session opens at the path defined by the environment variable \`RENKU_SESSION_PATH\`. If you set a subpath (e.g., "foo"), the session will open at \`<RENKU_SESSION_PATH>/foo\`.`,
   port: `The network port that your application will use to listen for incoming connections.  
