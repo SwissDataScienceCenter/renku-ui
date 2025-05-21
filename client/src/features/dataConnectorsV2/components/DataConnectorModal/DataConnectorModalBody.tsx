19a2348a3fec61718106cdc4a05ef28026d0444e
Andrea Cordoba <dandrea.cordoba@gmail.com>
2025-05-21 11:09:47 +0200

    Split edit data connector form

--- client/src/features/dataConnectorsV2/components/DataConnectorModal/DataConnectorModalBody.tsx
+++ client/src/features/dataConnectorsV2/components/DataConnectorModal/DataConnectorModalBody.tsx
@@ -161,12 +161,14 @@ function AddOrEditDataConnector({
   if (DataConnectorContentByStep)
     return (
       <>
-        <div className={cx("d-flex", "justify-content-end")}>
-          <AddStorageAdvancedToggle
-            state={cloudStorageState}
-            setState={setState}
-          />
-        </div>
+        {!flatDataConnector.dataConnectorId && (
+          <div className={cx("d-flex", "justify-content-end")}>
+            <AddStorageAdvancedToggle
+              state={cloudStorageState}
+              setState={setState}
+            />
+          </div>
+        )}
         <DataConnectorContentByStep
           dataConnector={dataConnector}
           project={project}
@@ -312,7 +314,7 @@ export function DataConnectorMount({
 
   return (
     <form className="form-rk-green" data-cy="data-connector-edit-mount">
-      <h5>Mount Properties</h5>
+      <h5 className="fw-bold">Mount properties</h5>
       <p>We need a few more details to mount your data properly.</p>
 
       <div className="mb-3">
