7d1e4ab04879b8df0fe44a2bcf8ab91f0cd5aafe
Lorenzo <lorenzo.cavazzi.tech@gmail.com>
2025-06-05 17:17:16 +0200

    TMP keywords

---
+++ client/src/components/keywords/KeywordBadge.tsx
@@ -0,0 +1,65 @@
+/*!
+ * Copyright 2025 - Swiss Data Science Center (SDSC)
+ * A partnership between École Polytechnique Fédérale de Lausanne (EPFL) and
+ * Eidgenössische Technische Hochschule Zürich (ETHZ).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import cx from "classnames";
+import RenkuBadge from "../renkuBadge/RenkuBadge";
+import { XCircle } from "react-bootstrap-icons";
+
+interface KeywordBadgeProps {
+  children?: React.ReactNode;
+  className?: string;
+  "data-cy"?: string;
+  highlighted?: boolean;
+  removable?: boolean;
+  removeHandler?: () => void;
+}
+
+export default function KeywordBadge({
+  children,
+  className,
+  "data-cy": dataCy = "keyword",
+  highlighted,
+  removable = true,
+  removeHandler,
+}: KeywordBadgeProps) {
+  const remove =
+    removable && removeHandler ? (
+      <XCircle
+        aria-label="Remove keyword"
+        className={cx("cursor-pointer")}
+        data-cy={`${dataCy}-remove`}
+        onClick={removeHandler}
+      />
+    ) : null;
+
+  return (
+    <RenkuBadge
+      className={cx(
+        "d-flex",
+        "fw-semibold",
+        "gap-1",
+        highlighted ? "bg-success-subtle" : "",
+        className
+      )}
+      data-cy={dataCy}
+    >
+      {children}
+      {remove}
+    </RenkuBadge>
+  );
+}
