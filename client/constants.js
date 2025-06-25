ef4703a13367f2e3b0c22a0354e0d96750b8d98c
Tasko Olevski <olevski90@gmail.com>
2025-06-25 15:54:06 +0200

    chore: remove startup file generation

---
+++ client/constants.js
@@ -0,0 +1,56 @@
+export const BASE_URL = process.env.BASE_URL;
+export const SITEMAP = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>${BASE_URL}</loc>
+  </url>
+  <url>
+    <loc>${BASE_URL}/search</loc>
+  </url>
+  <url>
+    <loc>${BASE_URL}/projects</loc>
+  </url>
+  <url>
+    <loc>${BASE_URL}/datasets</loc>
+  </url>
+  <url>
+    <loc>${BASE_URL}/help</loc>
+  </url>
+  <url>
+    <loc>${BASE_URL}/help/status</loc>
+  </url>
+  <url>
+    <loc>${BASE_URL}/help/release</loc>
+  </url>
+</urlset>`;
+export const ROBOTS = `Sitemap: ${BASE_URL} /sitemap.xml`;
+export const CONFIG_JSON = {
+  UI_VERSION: process.env.UI_VERSION || "",
+  RENKU_CHART_VERSION: process.env.RENKU_CHART_VERSION,
+  UI_SHORT_SHA: process.env.RENKU_UI_SHORT_SHA,
+  BASE_URL: process.env.BASE_URL || "http://renku.build",
+  GATEWAY_URL: process.env.GATEWAY_URL || "http://gateway.renku.build",
+  UISERVER_URL: process.env.UISERVER_URL || "http://uiserver.renku.build",
+  KEYCLOAK_REALM: process.env.KEYCLOAK_REALM || "Renku",
+  DASHBOARD_MESSAGE: process.env.DASHBOARD_MESSAGE,
+  SENTRY_URL: process.env.SENTRY_URL,
+  SENTRY_NAMESPACE: process.env.SENTRY_NAMESPACE,
+  SENTRY_SAMPLE_RATE: process.env.SENTRY_SAMPLE_RATE,
+  MAINTENANCE: process.env.MAINTENANCE,
+  ANONYMOUS_SESSIONS: process.env.ANONYMOUS_SESSIONS,
+  PRIVACY_BANNER_ENABLED: process.env.PRIVACY_BANNER_ENABLED,
+  PRIVACY_BANNER_CONTENT: process.env.PRIVACY_BANNER_CONTENT,
+  PRIVACY_BANNER_LAYOUT: process.env.PRIVACY_BANNER_LAYOUT,
+  TERMS_PAGES_ENABLED: process.env.TERMS_PAGES_ENABLED,
+  TERMS_CONTENT: process.env.TERMS_CONTENT || "",
+  TEMPLATES: process.env.TEMPLATES,
+  PREVIEW_THRESHOLD: process.env.PREVIEW_THRESHOLD,
+  UPLOAD_THRESHOLD: process.env.UPLOAD_THRESHOLD,
+  STATUSPAGE_ID: process.env.STATUSPAGE_ID,
+  HOMEPAGE: process.env.HOMEPAGE,
+  CORE_API_VERSION_CONFIG: process.env.CORE_API_VERSION_CONFIG,
+  USER_PREFERENCES_MAX_PINNED_PROJECTS:
+    process.env.USER_PREFERENCES_MAX_PINNED_PROJECTS,
+  SESSION_CLASS_EMAIL_US: process.env.SESSION_CLASS_EMAIL_US,
+  IMAGE_BUILDERS_ENABLED: process.env.IMAGE_BUILDERS_ENABLED,
+};
