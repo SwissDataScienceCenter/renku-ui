af37affea4f98972bdc0fc6099092f21af8fad0d
Lorenzo <lorenzo.cavazzi.tech@gmail.com>
2025-06-27 17:25:02 +0200

    Add a basic visualization for results

--- client/src/features/groupsV2/search/GroupSearchResults.tsx
+++ client/src/features/groupsV2/search/GroupSearchResults.tsx
@@ -18,12 +18,23 @@
 
 import cx from "classnames";
 import { useMemo } from "react";
+import {
+  Database,
+  Eye,
+  Folder2Open,
+  Globe2,
+  Lock,
+  Pencil,
+  Question,
+} from "react-bootstrap-icons";
 import { generatePath, Link, useSearchParams } from "react-router";
-import { ListGroup, ListGroupItem } from "reactstrap";
+import { Col, ListGroup, Row } from "reactstrap";
 import Pagination from "~/components/Pagination";
+import { TimeCaption } from "~/components/TimeCaption";
 import KeywordBadge from "~/components/keywords/KeywordBadge";
 import KeywordContainer from "~/components/keywords/KeywordContainer";
 import { ShowGlobalDataConnector } from "~/features/searchV2/components/SearchV2Results";
+import UserAvatar from "~/features/usersV2/show/UserAvatar";
 import { ABSOLUTE_ROUTES } from "~/routing/routes.constants";
 import { useGroupSearch } from "./groupSearch.hook";
 import { GroupSearchEntity } from "./groupSearch.types";
@@ -104,16 +115,99 @@ function SearchResultListItem({ item }: SearchResultListItemProps) {
       )}
       to={url}
     >
-      <ListGroupItem>
-        <h5 className="mb-1">{item.name}</h5>
-        <p>{item.path}</p>
-        <p>{item.slug}</p>
-        <KeywordContainer>
-          {sortedKeywords.map((keyword, index) => (
-            <KeywordBadge key={index}>{keyword}</KeywordBadge>
-          ))}
-        </KeywordContainer>
-      </ListGroupItem>
+      <Row className="g-2">
+        <Col xs="auto">
+          <h5>
+            <SearchResultListItemIcon item={item} />
+          </h5>
+        </Col>
+        <Col className={cx("d-flex", "flex-column", "gap-2")}>
+          <h5 className="mb-0">{item.name}</h5>
+          {item.createdBy && (
+            <p className={cx("align-items-center", "d-flex", "gap-2", "mb-0")}>
+              <span className="fst-italic">Created by</span>{" "}
+              <span className={cx("align-items-center", "d-flex", "gap-1")}>
+                <UserAvatar namespace={item.createdBy.slug} />{" "}
+                {item.createdBy.firstName} {item.createdBy.lastName}
+              </span>
+            </p>
+          )}
+          {item.description && <p className="mb-0">{item.description}</p>}
+          {sortedKeywords.length > 0 && (
+            <KeywordContainer>
+              {sortedKeywords.map((keyword, index) => (
+                <KeywordBadge key={index}>{keyword}</KeywordBadge>
+              ))}
+            </KeywordContainer>
+          )}
+          {item.type === "DataConnector" && (
+            <p
+              className={cx(
+                "align-items-center",
+                "d-flex",
+                "flex-wrap",
+                "gap-3",
+                "mb-0"
+              )}
+            >
+              <span>
+                <Database className={cx("bi", "me-1")} />
+                {item.storageType}
+              </span>{" "}
+              <span>
+                {item.readonly ? (
+                  <>
+                    <Eye className={cx("bi", "me-1")} />
+                    Read only
+                  </>
+                ) : (
+                  <>
+                    <Pencil className={cx("bi", "me-1")} />
+                    Read/Write
+                  </>
+                )}
+              </span>
+            </p>
+          )}
+          <div
+            className={cx(
+              "align-items-center",
+              "d-flex",
+              "flex-wrap",
+              "gap-2",
+              "justify-content-between"
+            )}
+          >
+            {item.visibility.toLowerCase() === "private" ? (
+              <div>
+                <Lock className={cx("bi", "me-1")} />
+                Private
+              </div>
+            ) : (
+              <div>
+                <Globe2 className={cx("bi", "me-1")} />
+                Public
+              </div>
+            )}
+            <TimeCaption
+              className="fs-6"
+              datetime={item.creationDate}
+              prefix="Created"
+              enableTooltip
+            />
+          </div>
+        </Col>
+      </Row>
     </Link>
+  );
+}
+
+function SearchResultListItemIcon({ item }: { item: GroupSearchEntity }) {
+  return item.type === "Project" ? (
+    <Folder2Open />
+  ) : item.type === "DataConnector" ? (
+    <Database />
+  ) : (
+    <Question />
   );
 }
