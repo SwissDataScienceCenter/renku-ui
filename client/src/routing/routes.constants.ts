6f35381da30812564f0aaf90a9beee9f27130797
Lorenzo <lorenzo.cavazzi.tech@gmail.com>
2025-07-18 16:28:03 +0200

    feat: add v2 documentation links

--- client/src/routing/routes.constants.ts
+++ client/src/routing/routes.constants.ts
@@ -68,6 +68,7 @@ export const ABSOLUTE_ROUTES = {
       release: "/help/release",
       tos: "/help/tos",
       privacy: "/help/privacy",
+      documentation: "/help/documentation",
     },
     projects: {
       show: {
@@ -130,6 +131,7 @@ export const RELATIVE_ROUTES = {
       release: "release",
       tos: "tos",
       privacy: "privacy",
+      documentation: "documentation",
     },
     projects: {
       root: "p/*",
