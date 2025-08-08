a70e07cc445997639da6db01c28c7f41b1be8201
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2025-08-08 13:51:59 +0200

    fix

--- client/server.js
+++ client/server.js
@@ -1,14 +1,24 @@
+/*!
+ * Copyright 2025 - Swiss Data Science Center (SDSC)
+ * A partnership between École Polytechnique Fédérale de Lausanne (EPFL) and
+ * Eidgenössische Technische Hochschule Zürich (ETHZ).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import compression from "compression";
 import express from "express";
 import morgan from "morgan";
-
-import {
-  CONFIG_JSON,
-  ROBOTS,
-  SAMPLE_PRIVACY_CONTENT,
-  SAMPLE_TERMS_CONTENT,
-  SITEMAP,
-} from "./server/constants";
 
 const BUILD_PATH = "./build/server/index.js";
 const DEVELOPMENT = process.env.NODE_ENV === "development";
@@ -35,29 +45,38 @@ app.use(
   express.static("build/client/assets", { immutable: true, maxAge: "1y" })
 );
 
-//Configuration and miscellaneous files
-app.get("/config.json", (_, res) => {
-  res.json(CONFIG_JSON);
-});
-app.get("/sitemap.xml", (_, res) => {
-  res.setHeader("Content-Type", "application/xml");
-  res.send(SITEMAP);
-});
-app.get("/robots.txt", (_, res) => {
-  res.send(ROBOTS);
-});
-if (CONFIG_JSON.TERMS_PAGES_ENABLED) {
-  app.get("/terms-of-use.md", (_, res) => {
-    CONFIG_JSON.TERMS_CONTENT.length > 0
-      ? res.send(CONFIG_JSON.TERMS_CONTENT)
-      : res.send(SAMPLE_TERMS_CONTENT);
+// Configuration and miscellaneous files
+await import(BUILD_PATH).then((mod) => {
+  const {
+    CONFIG_JSON,
+    ROBOTS,
+    SAMPLE_PRIVACY_CONTENT,
+    SAMPLE_TERMS_CONTENT,
+    SITEMAP,
+  } = mod.constants;
+  app.get("/config.json", (_, res) => {
+    res.json(CONFIG_JSON);
   });
-  app.get("/privacy-statement.md", (_, res) => {
-    CONFIG_JSON.PRIVACY_CONTENT.length > 0
-      ? res.send(CONFIG_JSON.PRIVACY_CONTENT)
-      : res.send(SAMPLE_PRIVACY_CONTENT);
+  app.get("/sitemap.xml", (_, res) => {
+    res.setHeader("Content-Type", "application/xml");
+    res.send(SITEMAP);
   });
-}
+  app.get("/robots.txt", (_, res) => {
+    res.send(ROBOTS);
+  });
+  if (CONFIG_JSON.TERMS_PAGES_ENABLED) {
+    app.get("/terms-of-use.md", (_, res) => {
+      CONFIG_JSON.TERMS_CONTENT.length > 0
+        ? res.send(CONFIG_JSON.TERMS_CONTENT)
+        : res.send(SAMPLE_TERMS_CONTENT);
+    });
+    app.get("/privacy-statement.md", (_, res) => {
+      CONFIG_JSON.PRIVACY_CONTENT.length > 0
+        ? res.send(CONFIG_JSON.PRIVACY_CONTENT)
+        : res.send(SAMPLE_PRIVACY_CONTENT);
+    });
+  }
+});
 
 // Logging
 app.use(morgan("tiny"));
