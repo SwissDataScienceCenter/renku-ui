b3893abdf0f064baa4c30a9a51294aab18f1e833
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2025-08-08 14:02:58 +0200

    add typing

--- client/server.js
+++ client/server.js
@@ -46,37 +46,44 @@ app.use(
 );
 
 // Configuration and miscellaneous files
-await import(BUILD_PATH).then((mod) => {
-  const {
-    CONFIG_JSON,
-    ROBOTS,
-    SAMPLE_PRIVACY_CONTENT,
-    SAMPLE_TERMS_CONTENT,
-    SITEMAP,
-  } = mod.constants;
-  app.get("/config.json", (_, res) => {
-    res.json(CONFIG_JSON);
-  });
-  app.get("/sitemap.xml", (_, res) => {
-    res.setHeader("Content-Type", "application/xml");
-    res.send(SITEMAP);
-  });
-  app.get("/robots.txt", (_, res) => {
-    res.send(ROBOTS);
-  });
-  if (CONFIG_JSON.TERMS_PAGES_ENABLED) {
-    app.get("/terms-of-use.md", (_, res) => {
-      CONFIG_JSON.TERMS_CONTENT.length > 0
-        ? res.send(CONFIG_JSON.TERMS_CONTENT)
-        : res.send(SAMPLE_TERMS_CONTENT);
+await import(BUILD_PATH).then(
+  (
+    /**
+     * @import * as ModuleType from './server/app'
+     * @type {ModuleType} */
+    mod
+  ) => {
+    const {
+      CONFIG_JSON,
+      ROBOTS,
+      SAMPLE_PRIVACY_CONTENT,
+      SAMPLE_TERMS_CONTENT,
+      SITEMAP,
+    } = mod.constants;
+    app.get("/config.json", (_, res) => {
+      res.json(CONFIG_JSON);
     });
-    app.get("/privacy-statement.md", (_, res) => {
-      CONFIG_JSON.PRIVACY_CONTENT.length > 0
-        ? res.send(CONFIG_JSON.PRIVACY_CONTENT)
-        : res.send(SAMPLE_PRIVACY_CONTENT);
+    app.get("/sitemap.xml", (_, res) => {
+      res.setHeader("Content-Type", "application/xml");
+      res.send(SITEMAP);
     });
+    app.get("/robots.txt", (_, res) => {
+      res.send(ROBOTS);
+    });
+    if (CONFIG_JSON.TERMS_PAGES_ENABLED) {
+      app.get("/terms-of-use.md", (_, res) => {
+        CONFIG_JSON.TERMS_CONTENT.length > 0
+          ? res.send(CONFIG_JSON.TERMS_CONTENT)
+          : res.send(SAMPLE_TERMS_CONTENT);
+      });
+      app.get("/privacy-statement.md", (_, res) => {
+        CONFIG_JSON.PRIVACY_CONTENT.length > 0
+          ? res.send(CONFIG_JSON.PRIVACY_CONTENT)
+          : res.send(SAMPLE_PRIVACY_CONTENT);
+      });
+    }
   }
-});
+);
 
 // Logging
 app.use(morgan("tiny"));
@@ -85,7 +92,16 @@ app.use(morgan("tiny"));
 app.use(express.static("build/client"));
 
 // Server-side rendering
-app.use(await import(BUILD_PATH).then((mod) => mod.app));
+app.use(
+  await import(BUILD_PATH).then(
+    (
+      /**
+       * @import * as ModuleType from './server/app'
+       * @type {ModuleType} */
+      mod
+    ) => mod.app
+  )
+);
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
