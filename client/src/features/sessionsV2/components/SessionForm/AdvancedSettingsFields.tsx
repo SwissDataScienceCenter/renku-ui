1d7d467a6b313f5bef96e51b5a600c84291a1482
Tasko Olevski <olevski90@gmail.com>
2025-08-22 16:47:03 +0200

    Update client/src/features/sessionsV2/components/SessionForm/AdvancedSettingsFields.tsx
    Co-authored-by: Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>

--- client/src/features/sessionsV2/components/SessionForm/AdvancedSettingsFields.tsx
+++ client/src/features/sessionsV2/components/SessionForm/AdvancedSettingsFields.tsx
@@ -78,28 +78,17 @@ function FormField<T extends FieldValues>({
         control={control}
         name={name}
         rules={rules}
-        render={({ field }) =>
-          type === "checkbox" ? (
-            <Input
-              className={cx(errors?.[name] && "is-invalid")}
-              data-cy={`session-launcher-field-${name}`}
-              id={`addSessionLauncher${name}`}
-              placeholder={placeholder}
-              type={type}
-              checked={field.value}
-              {...field}
-            />
-          ) : (
-            <Input
-              className={cx(errors?.[name] && "is-invalid")}
-              data-cy={`session-launcher-field-${name}`}
-              id={`addSessionLauncher${name}`}
-              placeholder={placeholder}
-              type={type}
-              {...field}
-            />
-          )
-        }
+        render={({ field }) => (
+          <Input
+            className={cx(errors?.[name] && "is-invalid")}
+            data-cy={`session-launcher-field-${name}`}
+            id={`addSessionLauncher${name}`}
+            placeholder={placeholder}
+            type={type}
+            {...(type === "checkbox" ? { checked: field.value } : {})}
+            {...field}
+          />
+        )}
       />
       {errors?.[name] && (
         <div className={cx("d-block", "invalid-feedback")}>
