cd4297f4748beb9e69705df56993da0d86448599
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2025-08-25 13:31:13 +0200

    fix layout for checkbox

--- client/src/features/sessionsV2/components/SessionForm/AdvancedSettingsFields.tsx
+++ client/src/features/sessionsV2/components/SessionForm/AdvancedSettingsFields.tsx
@@ -59,6 +59,22 @@ function FormField<T extends FieldValues>({
   type: InputType;
   isOptional?: boolean;
 }) {
+  if (type === "checkbox" || type === "radio") {
+    return (
+      <CheckboxOrRadioFormField
+        control={control}
+        errors={errors}
+        info={info}
+        label={label}
+        name={name}
+        placeholder={placeholder}
+        rules={rules}
+        type={type}
+        isOptional={isOptional}
+      />
+    );
+  }
+
   return (
     <>
       <Label
@@ -85,7 +101,6 @@ function FormField<T extends FieldValues>({
             id={`addSessionLauncher${name}`}
             placeholder={placeholder}
             type={type}
-            {...(type === "checkbox" ? { checked: field.value } : {})}
             {...field}
           />
         )}
@@ -98,6 +113,70 @@ function FormField<T extends FieldValues>({
         </div>
       )}
     </>
+  );
+}
+
+// NOTE: checkbox and radio inputs require a different layout.
+function CheckboxOrRadioFormField<T extends FieldValues>({
+  control,
+  errors,
+  info,
+  label,
+  name,
+  placeholder,
+  rules,
+  type = "text",
+  isOptional,
+}: {
+  control: Control<T>;
+  errors?: FieldErrors<T>;
+  info: string;
+  label: ReactNode;
+  name: Path<T>;
+  placeholder?: string;
+  rules?: ControllerProps<T>["rules"];
+  type: InputType;
+  isOptional?: boolean;
+}) {
+  return (
+    <div className="form-check">
+      <Controller
+        control={control}
+        name={name}
+        rules={rules}
+        render={({ field }) => (
+          <Input
+            className={cx(errors?.[name] && "is-invalid")}
+            data-cy={`session-launcher-field-${name}`}
+            id={`addSessionLauncher${name}`}
+            placeholder={placeholder}
+            type={type}
+            checked={field.value}
+            {...field}
+          />
+        )}
+      />
+      <Label
+        for={`addSessionLauncher${name}`}
+        className={cx("form-label", "me-2")}
+        aria-required={isOptional ? "false" : "true"}
+      >
+        {label}
+        {isOptional && (
+          <span className={cx("small", "text-muted", "ms-2")}>(Optional)</span>
+        )}
+      </Label>
+      <MoreInfo>
+        <LazyRenkuMarkdown markdownText={info} />
+      </MoreInfo>
+      {errors?.[name] && (
+        <div className={cx("d-block", "invalid-feedback")}>
+          {errors[name]?.message
+            ? errors[name]?.message?.toString()
+            : `Please provide a valid value for ${name}`}
+        </div>
+      )}
+    </div>
   );
 }
 
