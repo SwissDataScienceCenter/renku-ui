9fcfdb64d2edda8cfc54c61f3ea52f43bf7c2c1f
Chandrasekhar Ramakrishnan <ciyer+air@illposed.com>
2025-08-29 13:19:55 +0200

    feat: redirect on visits to registered legacy project urls

--- client/src/features/legacy/index.tsx
+++ client/src/features/legacy/index.tsx
@@ -1,9 +1,25 @@
+/*!
+ * Copyright 2025 - Swiss Data Science Center (SDSC)
+ * A partnership between École Polytechnique Fédérale de Lausanne (EPFL) and
+ * Eidgenössische Technische Hochschule Zürich (ETHZ).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import cx from "classnames";
 import { useContext } from "react";
-import { Link, Navigate } from "react-router";
-import { ArrowLeft } from "react-bootstrap-icons";
+import { Navigate } from "react-router";
 
-import ContainerWrap from "../../components/container/ContainerWrap";
 import { DatasetCoordinator } from "../../dataset/Dataset.state";
 import LazyShowDataset from "../../dataset/LazyShowDataset";
 import LazyProjectView from "../../project/LazyProjectView";
@@ -15,51 +31,8 @@ import LazyRootV1 from "../rootV1/LazyRootV1";
 import NavbarV2 from "../rootV2/NavbarV2";
 import type { UserInfo } from "../usersV2/api/users.types";
 
-function NoLegacySupport() {
-  const title = "Legacy not supported";
-  const description = "Renku Legacy is not supported in this deployment.";
-  const descriptionType = typeof description;
-  const Tag =
-    descriptionType === "string" ||
-    descriptionType === "number" ||
-    descriptionType === "boolean"
-      ? "p"
-      : "div";
-
-  const homeLink = "/";
-  return (
-    <ContainerWrap>
-      <div className={cx("d-flex")}>
-        <div className={cx("m-auto", "d-flex", "flex-column")}>
-          <h3
-            data-cy="not-found-title"
-            className={cx(
-              "fw-bold",
-              "mt-0",
-              "mb-3",
-              "d-flex",
-              "align-items-center",
-              "gap-3",
-              "text-primary"
-            )}
-          >
-            {/*
-            TODO: Put a nice image here when available
-            <img src={rkNotFoundImg} /> */}
-            {title}
-          </h3>
-          <Tag data-cy="not-found-description">{description}</Tag>
-          <div>
-            <Link to={homeLink} className={cx("btn", "btn-primary")}>
-              <ArrowLeft className={cx("bi", "me-1")} />
-              Return to home
-            </Link>
-          </div>
-        </div>
-      </div>
-    </ContainerWrap>
-  );
-}
+import CheckForRedirect from "./CheckForRedirect";
+import NoLegacySupport from "./NoLegacySupport";
 
 export function LegacyDatasetAddToProject() {
   const { params } = useContext(AppContext);
@@ -90,7 +63,7 @@ export function LegacyDatasets() {
 export function LegacyProjectView() {
   const { params } = useContext(AppContext);
   if (params && !params.LEGACY_SUPPORT.enabled) {
-    return <NoLegacySupport />;
+    return <CheckForRedirect />;
   }
 
   return <LazyProjectView />;
