f7be757ec5d42c8708b6505e2344f7f3247f78bc
Wes Johnson <wes@wes-johnson.com>
2025-10-02 15:09:38 +0200

    more linting fixes

--- server/src/websocket/handlers/prometheus.ts
+++ server/src/websocket/handlers/prometheus.ts
@@ -19,7 +19,6 @@
 import fetch from "cross-fetch";
 import ws from "ws";
 
-import config from "../../config";
 import logger from "../../logger";
 import { WsMessage } from "../WsMessages";
 import type { Channel } from "./handlers.types";
@@ -84,64 +83,6 @@ async function executePrometheusFullPath(
     const responseData = await prometheusResponse.json();
     logger.info(`📈 Response data:`, JSON.stringify(responseData, null, 2));
 
-    const successMessage = new WsMessage(
-      {
-        ...responseData,
-        requestId,
-      },
-      "user",
-      "prometheusQuery"
-    ).toString();
-    socket.send(successMessage);
-  } catch (error) {
-    logger.error("Error executing Prometheus query:", error);
-
-    const failureMessage = new WsMessage(
-      {
-        error: "Error executing Prometheus query",
-        details: error.message,
-        requestId,
-      },
-      "user",
-      "prometheusQuery"
-    ).toString();
-    socket.send(failureMessage);
-  }
-}
-
-async function executePrometheusQuery(
-  query: string,
-  requestId: string,
-  socket: ws
-): Promise<void> {
-  try {
-    const prometheusUrl = new URL("/api/v1/query", config.prometheus.url);
-    prometheusUrl.searchParams.set("query", query);
-
-    const prometheusResponse = await fetch(prometheusUrl.toString(), {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-      },
-    });
-
-    if (!prometheusResponse.ok) {
-      logger.error(
-        `Prometheus query failed with status ${prometheusResponse.status}`
-      );
-      const errorMessage = new WsMessage(
-        {
-          error: `Prometheus server error: ${prometheusResponse.statusText}`,
-          requestId,
-        },
-        "user",
-        "prometheusQuery"
-      ).toString();
-      socket.send(errorMessage);
-      return;
-    }
-
-    const responseData = await prometheusResponse.json();
     const successMessage = new WsMessage(
       {
         ...responseData,
