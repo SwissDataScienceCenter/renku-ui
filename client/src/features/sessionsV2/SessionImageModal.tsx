da80b9312fac6ae3d8c1e63182ad7714d7d3e495
Lorenzo <lorenzo.cavazzi.tech@gmail.com>
2025-10-07 15:43:55 +0200

    prefer "image reference" over "image URL"

--- client/src/features/sessionsV2/SessionImageModal.tsx
+++ client/src/features/sessionsV2/SessionImageModal.tsx
@@ -80,11 +80,11 @@ export default function SessionImageModal({
         {!data.connection && !data.provider ? (
           <>
             <p className="mb-2">
-              The image URL <code>{containerImage}</code> is invalid or points
-              to an unsupported registry.
+              The image reference <code>{containerImage}</code> is invalid or
+              points to an unsupported registry.
             </p>
             <p className="mb-0">
-              Please verify the URL and check if the registry is in the
+              Please verify the reference and check if the registry is in the
               currently supported{" "}
               <Link
                 to={{
@@ -95,7 +95,7 @@ export default function SessionImageModal({
                 <Plugin className={cx("bi", "me-1")} />
                 integrations
               </Link>
-              . If you{"'"}re certain the URL is correct and points to a
+              . If you{"'"}re certain the reference is correct and points to a
               registry we don{"'"}t currently support,{" "}
               <a
                 target="_blank"
@@ -111,7 +111,8 @@ export default function SessionImageModal({
         ) : data.connection?.status === "connected" ? (
           <>
             <p className="mb-0">
-              Either the image does not exist, or you do not have access to it.
+              Either the image reference does not exist, or you do not have
+              access to it.
             </p>
             {data?.provider?.id && (
               <>
@@ -135,9 +136,9 @@ export default function SessionImageModal({
         ) : (
           <>
             <p className="mb-2">
-              This image is from a supported registry, but you haven{"'"}t
-              activated the integration yet. Activate the integration to check
-              if you have access to this image.
+              This image reference is from a supported registry, but you haven
+              {"'"}t activated the integration yet. Activate the integration to
+              check if you have access to this image.
             </p>
             <Link
               className={cx("btn", "btn-primary", "btn-sm")}
