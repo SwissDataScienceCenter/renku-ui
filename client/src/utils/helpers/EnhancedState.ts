e613637cff985c9ab628f231aa37bace8a0f74b5
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2025-12-10 14:59:41 +0100

    fix: add notificationsApi to EnhancedState.ts

--- client/src/utils/helpers/EnhancedState.ts
+++ client/src/utils/helpers/EnhancedState.ts
@@ -28,6 +28,7 @@ import {
   StoreEnhancer,
 } from "@reduxjs/toolkit";
 
+import { notificationsEmptyApi as notificationsApi } from "~/features/notifications/api/notifications.empty-api";
 import { computeResourcesEmptyApi as computeResourcesApi } from "~/features/sessionsV2/api/computeResources.empty-api";
 import adminKeycloakApi from "../../features/admin/adminKeycloak.api";
 import { connectedServicesEmptyApi as connectedServicesApi } from "../../features/connectedServices/api/connectedServices.empty-api";
@@ -95,6 +96,7 @@ export const createStore = <S = any, A extends Action = AnyAction>(
     [doiResolverApi.reducerPath]: doiResolverApi.reducer,
     [inactiveKgProjectsApi.reducerPath]: inactiveKgProjectsApi.reducer,
     [kgSearchApi.reducerPath]: kgSearchApi.reducer,
+    [notificationsApi.reducerPath]: notificationsApi.reducer,
     [platformApi.reducerPath]: platformApi.reducer,
     [projectCloudStorageApi.reducerPath]: projectCloudStorageApi.reducer,
     [projectCoreApi.reducerPath]: projectCoreApi.reducer,
