2ad27bfa5ee03ff676f5549f7b9b146676ce4c5f
Andrea Cordoba <dandrea.cordoba@gmail.com>
2026-01-29 10:06:12 +0100

    fix tests

--- client/src/features/sessionsV2/api/sessionsV2.generated-api.ts
+++ client/src/features/sessionsV2/api/sessionsV2.generated-api.ts
@@ -2,82 +2,6 @@ import { sessionsV2EmptyApi as api } from "./sessionsV2.empty-api";
 
 const injectedRtkApi = api.injectEndpoints({
   endpoints: (build) => ({
-    getNotebooksImages: build.query<
-      GetNotebooksImagesApiResponse,
-      GetNotebooksImagesApiArg
-    >({
-      query: (queryArg) => ({
-        url: `/notebooks/images`,
-        params: { image_url: queryArg.imageUrl },
-      }),
-    }),
-    getNotebooksLogsByServerName: build.query<
-      GetNotebooksLogsByServerNameApiResponse,
-      GetNotebooksLogsByServerNameApiArg
-    >({
-      query: (queryArg) => ({
-        url: `/notebooks/logs/${queryArg.serverName}`,
-        params: { max_lines: queryArg.maxLines },
-      }),
-    }),
-    getNotebooksServerOptions: build.query<
-      GetNotebooksServerOptionsApiResponse,
-      GetNotebooksServerOptionsApiArg
-    >({
-      query: () => ({ url: `/notebooks/server_options` }),
-    }),
-    postNotebooksServers: build.mutation<
-      PostNotebooksServersApiResponse,
-      PostNotebooksServersApiArg
-    >({
-      query: (queryArg) => ({
-        url: `/notebooks/servers`,
-        method: "POST",
-        body: queryArg.launchNotebookRequestOld,
-      }),
-    }),
-    getNotebooksServers: build.query<
-      GetNotebooksServersApiResponse,
-      GetNotebooksServersApiArg
-    >({
-      query: (queryArg) => ({
-        url: `/notebooks/servers`,
-        params: {
-          project: queryArg.project,
-          commit_sha: queryArg.commitSha,
-          namespace: queryArg["namespace"],
-          branch: queryArg.branch,
-        },
-      }),
-    }),
-    deleteNotebooksServersByServerName: build.mutation<
-      DeleteNotebooksServersByServerNameApiResponse,
-      DeleteNotebooksServersByServerNameApiArg
-    >({
-      query: (queryArg) => ({
-        url: `/notebooks/servers/${queryArg.serverName}`,
-        method: "DELETE",
-        params: { forced: queryArg.forced },
-      }),
-    }),
-    getNotebooksServersByServerName: build.query<
-      GetNotebooksServersByServerNameApiResponse,
-      GetNotebooksServersByServerNameApiArg
-    >({
-      query: (queryArg) => ({
-        url: `/notebooks/servers/${queryArg.serverName}`,
-      }),
-    }),
-    patchNotebooksServersByServerName: build.mutation<
-      PatchNotebooksServersByServerNameApiResponse,
-      PatchNotebooksServersByServerNameApiArg
-    >({
-      query: (queryArg) => ({
-        url: `/notebooks/servers/${queryArg.serverName}`,
-        method: "PATCH",
-        body: queryArg.patchServerRequest,
-      }),
-    }),
     postSessions: build.mutation<PostSessionsApiResponse, PostSessionsApiArg>({
       query: (queryArg) => ({
         url: `/sessions`,
@@ -135,59 +59,6 @@ const injectedRtkApi = api.injectEndpoints({
   overrideExisting: false,
 });
 export { injectedRtkApi as sessionsV2GeneratedApi };
-export type GetNotebooksImagesApiResponse =
-  /** status 200 The image check has completed successfully */ ImageCheckResponse;
-export type GetNotebooksImagesApiArg = {
-  /** The Docker image URL (tag included) that should be fetched. */
-  imageUrl: string;
-};
-export type GetNotebooksLogsByServerNameApiResponse =
-  /** status 200 Server logs. An array of strings where each element is a line of the logs. */ ServerLogs;
-export type GetNotebooksLogsByServerNameApiArg = {
-  /** The name of the server whose logs should be fetched. */
-  serverName: ServerName;
-  /** The maximum number of (most recent) lines to return from the logs. */
-  maxLines?: number;
-};
-export type GetNotebooksServerOptionsApiResponse =
-  /** status 200 Server options such as CPU, memory, storage, etc. */ ServerOptionsEndpointResponse;
-export type GetNotebooksServerOptionsApiArg = void;
-export type PostNotebooksServersApiResponse =
-  /** status 201 The project was created */ NotebookResponse;
-export type PostNotebooksServersApiArg = {
-  launchNotebookRequestOld: LaunchNotebookRequestOld;
-};
-export type GetNotebooksServersApiResponse =
-  /** status 200 Map of all servers for a user. */ ServersGetResponse;
-export type GetNotebooksServersApiArg = {
-  project?: string;
-  commitSha?: string;
-  namespace?: string;
-  branch?: string;
-};
-export type DeleteNotebooksServersByServerNameApiResponse =
-  /** status 204 The server was stopped successfully. */ void;
-export type DeleteNotebooksServersByServerNameApiArg = {
-  /** The name of the server that should be deleted. */
-  serverName: ServerName;
-  /** If true, delete immediately disregarding the grace period
-    of the underlying JupyterServer resource.
-     */
-  forced?: boolean;
-};
-export type GetNotebooksServersByServerNameApiResponse =
-  /** status 200 Server properties. */ NotebookResponse;
-export type GetNotebooksServersByServerNameApiArg = {
-  /** The name of the server for which additional information is required. */
-  serverName: ServerName;
-};
-export type PatchNotebooksServersByServerNameApiResponse =
-  /** status 200 The server was patched successfully. */ NotebookResponse;
-export type PatchNotebooksServersByServerNameApiArg = {
-  /** The name of the server that should be patched. */
-  serverName: ServerName;
-  patchServerRequest: PatchServerRequest;
-};
 export type PostSessionsApiResponse =
   /** status 200 The session already exists */
   SessionResponse | /** status 201 The session was created */ SessionResponse;
@@ -230,191 +101,7 @@ export type GetSessionsImagesApiArg = {
   /** The Docker image URL (tag included) that should be fetched. */
   imageUrl: string;
 };
-export type ImagePlatform = {
-  architecture: string;
-  os: string;
-  "os.version"?: string;
-  "os.features"?: string[];
-  variant?: string;
-};
-export type ImagePlatforms = ImagePlatform[];
-export type ImageConnectionStatus =
-  | "connected"
-  | "pending"
-  | "invalid_credentials";
-export type ImageConnection = {
-  id: string;
-  provider_id: string;
-  status: ImageConnectionStatus;
-};
-export type ImageProvider = {
-  id: string;
-  name: string;
-  url: string;
-};
-export type ImageCheckResponse = {
-  /** Whether the image is accessible or not. */
-  accessible: boolean;
-  platforms?: ImagePlatforms;
-  connection?: ImageConnection;
-  provider?: ImageProvider;
-};
-export type ErrorResponse = {
-  error: {
-    code: number;
-    detail?: string;
-    message: string;
-  };
-};
-export type ServerLogs = {
-  "jupyter-server"?: string;
-  [key: string]: any;
-};
 export type ServerName = string;
-export type Generated = {
-  enabled: boolean;
-};
-export type StringServerOptionsChoice = {
-  default: string;
-  displayName: string;
-  options?: string[];
-  order: number;
-  type: "enum" | "boolean";
-};
-export type BoolServerOptionsChoice = {
-  default: boolean;
-  displayName: string;
-  order: number;
-  type: "enum" | "boolean";
-};
-export type ServerOptionsEndpointResponse = {
-  cloudstorage: Generated;
-  defaultUrl?: StringServerOptionsChoice;
-  lfs_auto_fetch?: BoolServerOptionsChoice;
-};
-export type UserPodAnnotations = {
-  "jupyter.org/servername"?: string;
-  "jupyter.org/username"?: string;
-  "renku.io/branch": string;
-  "renku.io/commit-sha": string;
-  "renku.io/default_image_used": string;
-  "renku.io/git-host"?: string;
-  "renku.io/gitlabProjectId"?: string;
-  "renku.io/hibernatedSecondsThreshold"?: string;
-  "renku.io/hibernation"?: string;
-  "renku.io/hibernationBranch"?: string;
-  "renku.io/hibernationCommitSha"?: string;
-  "renku.io/hibernationDate"?: string;
-  "renku.io/hibernationDirty"?: string;
-  "renku.io/hibernationSynchronized"?: string;
-  "renku.io/idleSecondsThreshold"?: string;
-  "renku.io/lastActivityDate"?: string;
-  "renku.io/launcherId"?: string;
-  "renku.io/namespace": string;
-  "renku.io/projectId"?: string;
-  "renku.io/projectName": string;
-  "renku.io/renkuVersion"?: string;
-  "renku.io/repository": string;
-  "renku.io/resourceClassId"?: string;
-  "renku.io/servername"?: string;
-  "renku.io/username"?: string;
-  [key: string]: any;
-};
-export type LaunchNotebookResponseCloudStorage = {
-  mount_folder?: any;
-  remote?: any;
-  type?: any;
-};
-export type ResourceRequests = {
-  cpu: any;
-  gpu?: any;
-  memory: any;
-  storage?: any;
-};
-export type ResourceUsage = {
-  cpu?: any;
-  memory?: any;
-  storage?: any;
-};
-export type UserPodResources = {
-  requests?: ResourceRequests;
-  usage?: ResourceUsage;
-};
-export type ServerStatusDetail = {
-  status: "ready" | "waiting" | "executing" | "failed";
-  step: string;
-};
-export type ServerStatusWarning = {
-  critical?: boolean;
-  message: string;
-};
-export type ServerStatus = {
-  details: ServerStatusDetail[];
-  message?: string;
-  readyNumContainers: number;
-  state: "running" | "starting" | "stopping" | "failed" | "hibernated";
-  totalNumContainers: number;
-  warnings?: ServerStatusWarning[];
-};
-export type NotebookResponse = {
-  annotations?: UserPodAnnotations;
-  cloudstorage?: LaunchNotebookResponseCloudStorage[];
-  image?: string;
-  name?: ServerName;
-  resources?: UserPodResources;
-  started?: string | null;
-  state?: object;
-  status?: ServerStatus;
-  url?: string;
-};
-export type RCloneStorageRequest = {
-  configuration?: {
-    [key: string]: any;
-  } | null;
-  readonly?: boolean;
-  source_path?: string;
-  storage_id?: string | null;
-  target_path?: string;
-};
-export type LaunchNotebookRequestServerOptions = {
-  cpu_request?: any;
-  defaultUrl?: string;
-  disk_request?: any;
-  gpu_request?: any;
-  lfs_auto_fetch?: boolean;
-  mem_request?: any;
-};
-export type UserSecrets = {
-  mount_path: any;
-  user_secret_ids: any[];
-};
-export type LaunchNotebookRequestOld = {
-  branch?: string;
-  cloudstorage?: RCloneStorageRequest[];
-  commit_sha: string;
-  default_url?: string;
-  environment_variables?: {
-    [key: string]: string;
-  };
-  image?: string | null;
-  lfs_auto_fetch?: boolean;
-  namespace: string;
-  notebook?: string | null;
-  project: string;
-  resource_class_id?: number | null;
-  serverOptions?: LaunchNotebookRequestServerOptions;
-  storage?: number;
-  user_secrets?: UserSecrets | null;
-};
-export type ServersGetResponse = {
-  servers?: {
-    [key: string]: NotebookResponse;
-  };
-};
-export type PatchServerRequest = {
-  resource_class_id?: number;
-  state?: "running" | "hibernated";
-};
 export type SessionResourcesRequests = {
   /** Fractional CPUs */
   cpu?: number;
@@ -448,6 +135,13 @@ export type SessionResponse = {
   project_id: Ulid;
   launcher_id: Ulid;
   resource_class_id: number;
+};
+export type ErrorResponse = {
+  error: {
+    code: number;
+    detail?: string;
+    message: string;
+  };
 };
 export type RCloneConfig = {
   [key: string]: number | (string | null) | boolean | object;
@@ -488,15 +182,36 @@ export type SessionPatchRequest = {
 export type SessionLogsResponse = {
   [key: string]: string;
 };
+export type ImagePlatform = {
+  architecture: string;
+  os: string;
+  "os.version"?: string;
+  "os.features"?: string[];
+  variant?: string;
+};
+export type ImagePlatforms = ImagePlatform[];
+export type ImageConnectionStatus =
+  | "connected"
+  | "pending"
+  | "invalid_credentials";
+export type ImageConnection = {
+  id: string;
+  provider_id: string;
+  status: ImageConnectionStatus;
+};
+export type ImageProvider = {
+  id: string;
+  name: string;
+  url: string;
+};
+export type ImageCheckResponse = {
+  /** Whether the image is accessible or not. */
+  accessible: boolean;
+  platforms?: ImagePlatforms;
+  connection?: ImageConnection;
+  provider?: ImageProvider;
+};
 export const {
-  useGetNotebooksImagesQuery,
-  useGetNotebooksLogsByServerNameQuery,
-  useGetNotebooksServerOptionsQuery,
-  usePostNotebooksServersMutation,
-  useGetNotebooksServersQuery,
-  useDeleteNotebooksServersByServerNameMutation,
-  useGetNotebooksServersByServerNameQuery,
-  usePatchNotebooksServersByServerNameMutation,
   usePostSessionsMutation,
   useGetSessionsQuery,
   useGetSessionsBySessionIdQuery,
