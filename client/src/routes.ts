22b80ccde79d1dc23f5fde42f0ba2b3e9e9c232b
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2026-02-06 17:17:41 +0100

    remove sentry test routes

--- client/src/routes.ts
+++ client/src/routes.ts
@@ -3,6 +3,4 @@ import { route, type RouteConfig } from "@react-router/dev/routes";
 export default [
   // * matches all URLs, the ? makes it optional so it will match / as well
   route("*?", "routes/catchall.tsx"),
-  route("/sentry-example-page", "routes/sentry-example-page.tsx"),
-  route("/ui/api/sentry-example-api", "routes/api.sentry-example-api.ts"),
 ] satisfies RouteConfig;
