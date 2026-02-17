3d8ba47c70928831446dee5996f618f58a6f6fdf
Lorenzo Cavazzi <43481553+lorenzo-cavazzi@users.noreply.github.com>
2026-02-17 11:49:10 +0100

    fix: adjust link clicking area (#4022)

--- client/src/features/dataConnectorsV2/components/DataConnectorsBoxListDisplay.tsx
+++ client/src/features/dataConnectorsV2/components/DataConnectorsBoxListDisplay.tsx
@@ -128,20 +128,23 @@ export default function DataConnectorBoxListDisplay({
 
   return (
     <>
-      <ListGroupItem action data-cy="data-connector-item">
+      <ListGroupItem
+        action
+        className={cx("position-relative", "p-0")}
+        data-cy="data-connector-item"
+      >
         <Link
           className={cx(
-            "d-flex",
-            "flex-column",
-            "gap-3",
-            "link-primary",
+            "d-block",
             "text-body",
-            "text-decoration-none"
+            "text-decoration-none",
+            "link-primary",
+            "py-3"
           )}
           to={targetOffcanvasLocation}
         >
-          <Row className={cx("align-items-center", "g-3")}>
-            <Col className={cx("d-flex", "flex-column")}>
+          <Row className={cx("align-items-center", "g-3", "mx-0")}>
+            <Col className={cx("d-flex", "flex-column", "min-w-0", "px-0")}>
               <span className="fw-bold" data-cy="data-connector-name">
                 {name}
               </span>
@@ -199,10 +202,16 @@ export default function DataConnectorBoxListDisplay({
               </div>
             </Col>
             {/* This column is a placeholder to reserve the space for the action button */}
-            <Col xs="auto">
+            <Col xs="auto" className="flex-shrink-0">
               <div
                 aria-hidden="true"
-                className={cx("btn", "btn-sm", "opacity-0", "text-nowrap")}
+                className={cx(
+                  "btn",
+                  "btn-sm",
+                  "opacity-0",
+                  "pe-none",
+                  "text-nowrap"
+                )}
               >
                 FakeButton123
               </div>
