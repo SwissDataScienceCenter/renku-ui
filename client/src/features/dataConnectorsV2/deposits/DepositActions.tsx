095eb0e73af9cb1e05592e6f10ea3800a11aff78
Lorenzo <lorenzo.cavazzi.tech@gmail.com>
2026-03-06 14:33:49 +0100

    add deposit finalization

--- client/src/features/dataConnectorsV2/deposits/DepositActions.tsx
+++ client/src/features/dataConnectorsV2/deposits/DepositActions.tsx
@@ -3,6 +3,7 @@ import cx from "classnames";
 import { useCallback, useEffect, useState } from "react";
 import {
   ArrowRepeat,
+  Check2,
   DatabaseLock,
   FileEarmarkText,
   Pencil,
@@ -21,11 +22,13 @@ import {
 
 import DropdownButton from "~/components/buttons/DropdownButton";
 import RtkOrDataServicesError from "~/components/errors/RtkOrDataServicesError";
+import ExternalLink from "~/components/ExternalLink";
 import { Loader } from "~/components/Loader";
 import { Deposit } from "../api/data-connectors.api";
 import {
   useDeleteDepositsByDepositIdMutation,
   useGetDepositsByDepositIdQuery,
+  usePatchDepositsByDepositIdMutation,
 } from "../api/data-connectors.enhanced-api";
 import DepositEditModal from "./DepositEditModal";
 
@@ -353,6 +356,24 @@ function DepositFinalizationModal({
   toggleModal,
   isOpen,
 }: DepositFinalizationModalProps) {
+  const [patchDeposit, result] = usePatchDepositsByDepositIdMutation();
+
+  const finalizeDeposit = useCallback(() => {
+    patchDeposit({
+      depositId: deposit!.id ?? "",
+      depositPatch: {
+        status: "complete",
+      },
+    });
+  }, [deposit, patchDeposit]);
+
+  useEffect(() => {
+    if (!result.isSuccess || !isOpen) {
+      return;
+    }
+    toggleModal();
+  }, [isOpen, result.isSuccess, toggleModal]);
+
   return (
     <Modal size="lg" isOpen={isOpen} toggle={toggleModal} centered>
       <ModalHeader tag="h2" toggle={toggleModal}>
@@ -361,11 +382,44 @@ function DepositFinalizationModal({
       </ModalHeader>
       <ModalBody>
         <p>
-          Finalize deposit <strong>{deposit.name}</strong>
+          The job to export data to <strong>{deposit.name}</strong> has been
+          completed successfully. A final step on the target platform is usually
+          required to make the deposit publicly available.
         </p>
-        <p>Deposit finalization is not implemented yet.</p>
+        {deposit.external_url && (
+          <p>
+            Please follow the link{" "}
+            <ExternalLink href={deposit.external_url}>
+              {deposit.external_url}
+            </ExternalLink>{" "}
+            and check for pending actions required to finalize the export.
+          </p>
+        )}
+        <p>
+          Once the deposit is finalized on the target platform, you can come
+          here and click on the button Finalize deposit. You can use the DOI to
+          import it as a Data Connector here on Renku.
+        </p>
+        {result.error && <RtkOrDataServicesError error={result.error} />}
       </ModalBody>
       <ModalFooter>
+        <Button
+          color="primary"
+          disabled={result.isLoading}
+          onClick={finalizeDeposit}
+        >
+          {result.isLoading ? (
+            <>
+              <Loader className="me-1" inline size={16} />
+              Finalizing deposit...
+            </>
+          ) : (
+            <>
+              <Check2 className={cx("bi", "me-1")} />
+              Finalize deposit
+            </>
+          )}
+        </Button>
         <Button color="outline-primary" onClick={toggleModal}>
           <XLg className={cx("bi", "me-1")} />
           Close
