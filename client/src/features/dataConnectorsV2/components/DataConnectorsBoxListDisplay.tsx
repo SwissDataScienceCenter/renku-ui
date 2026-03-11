4e7e555c80018adf9ead43650f6c9ba7b1d4d4e3
Lorenzo <lorenzo.cavazzi.tech@gmail.com>
2026-03-11 16:21:08 +0100

    let it poll

--- client/src/features/dataConnectorsV2/components/DataConnectorsBoxListDisplay.tsx
+++ client/src/features/dataConnectorsV2/components/DataConnectorsBoxListDisplay.tsx
@@ -143,7 +143,6 @@ export default function DataConnectorBoxListDisplay({
     depositQueryArg,
     {
       pollingInterval: depositPollingInterval,
-      skipPollingIfUnfocused: true,
     }
   );
 
