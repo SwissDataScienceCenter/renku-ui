5a246c59ba13ea04f8721b8859ad3b597f115203
Lorenzo Cavazzi <43481553+lorenzo-cavazzi@users.noreply.github.com>
2026-04-21 12:39:51 +0200

    chore: show link to documentation in search pages (#4131)

--- client/src/utils/constants/NewDocs.ts
+++ client/src/utils/constants/NewDocs.ts
@@ -96,6 +96,10 @@ export const NEW_DOCS_HOW_RENKU_WORKS = newDocsLinkPage(
   "docs/users/knowledge-base/about"
 )(DEFAULT_NEW_DOC_LINK_ARGS);
 
+export const NEW_DOCS_SEARCH_QUERY = newDocsLinkPage(
+  "docs/users/search/search-query"
+)(DEFAULT_NEW_DOC_LINK_ARGS);
+
 export const NEW_DOCS_DATA_CONNECTORS = newDocsLinkPage("docs/users/data/data")(
   DEFAULT_NEW_DOC_LINK_ARGS
 );
