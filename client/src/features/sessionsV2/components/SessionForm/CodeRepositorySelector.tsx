4b4e84908e0d94483e0afba86ced749778e368a9
Samuel Gaist <samuel.gaist@idiap.ch>
2026-06-18 11:31:49 +0200

    refactor: disable code repository selection with proper message
    This makes the workflow more consistent for the user.

--- client/src/features/sessionsV2/components/SessionForm/CodeRepositorySelector.tsx
+++ client/src/features/sessionsV2/components/SessionForm/CodeRepositorySelector.tsx
@@ -17,7 +17,7 @@
  */
 
 import cx from "classnames";
-import { useCallback, useEffect, useMemo } from "react";
+import { useCallback, useContext, useEffect, useMemo } from "react";
 import { ChevronDown, XLg } from "react-bootstrap-icons";
 import {
   Controller,
@@ -38,6 +38,8 @@ import Select, {
 import { Label } from "reactstrap";
 
 import { GetRepositoriesApiResponse } from "~/features/repositories/api/repositories.api";
+import AppContext from "../../../../utils/context/appContext";
+import { DEFAULT_APP_PARAMS } from "../../../../utils/context/appParams.constants";
 import { getRepositoryName } from "../../../ProjectPageV2/ProjectPageContent/CodeRepositories/repositories.utils";
 
 import styles from "./Select.module.scss";
@@ -129,6 +131,11 @@ function CodeRepositorySelect({
   value: value_,
   disabled,
 }: CodeRepositorySelectProps) {
+  const { params } = useContext(AppContext);
+  const privateRepoBuildEnabled =
+    params?.BUILD_PRIVATE_REPO_BUILDS_ENABLED ??
+    DEFAULT_APP_PARAMS.BUILD_PRIVATE_REPO_BUILDS_ENABLED;
+
   const defaultValue = useMemo(
     () => options.find((repository) => repository.url === defaultValue_),
     [defaultValue_, options],
@@ -165,7 +172,9 @@ function CodeRepositorySelect({
       unstyled
       isOptionDisabled={(option) =>
         option.data?.status !== "valid" ||
-        !option.data.metadata?.pull_permission
+        !option.data.metadata?.pull_permission ||
+        (option.data.metadata.visibility == "private" &&
+          !privateRepoBuildEnabled)
       }
       onChange={onChange}
       onBlur={onBlur}
@@ -207,6 +216,11 @@ interface OptionValueContentProps {
 
 function OptionValueContent({ option, isDisabled }: OptionValueContentProps) {
   const title = getRepositoryName(option.url);
+  const { params } = useContext(AppContext);
+  const privateRepoBuildEnabled =
+    params?.BUILD_PRIVATE_REPO_BUILDS_ENABLED ??
+    DEFAULT_APP_PARAMS.BUILD_PRIVATE_REPO_BUILDS_ENABLED;
+
   return (
     <>
       <div>
@@ -218,12 +232,19 @@ function OptionValueContent({ option, isDisabled }: OptionValueContentProps) {
         >
           {title}
         </span>
-        {isDisabled && (
-          <span className="ms-1">
-            <XLg className={cx("bi", "me-1")} />
-            No public access
-          </span>
-        )}
+        {isDisabled &&
+          (option.data?.metadata?.visibility == "private" &&
+          !privateRepoBuildEnabled ? (
+            <span className="ms-1">
+              <XLg className={cx("bi", "me-1")} />
+              Private repository builds are not available
+            </span>
+          ) : (
+            <span className="ms-1">
+              <XLg className={cx("bi", "me-1")} />
+              No public access
+            </span>
+          ))}
       </div>
       <div>{option.url}</div>
     </>
@@ -236,17 +257,29 @@ interface SingleValueContentProps {
 }
 
 function SingleValueContent({ option, isDisabled }: SingleValueContentProps) {
+  const { params } = useContext(AppContext);
+  const privateRepoBuildEnabled =
+    params?.BUILD_PRIVATE_REPO_BUILDS_ENABLED ??
+    DEFAULT_APP_PARAMS.BUILD_PRIVATE_REPO_BUILDS_ENABLED;
+
   return (
     <>
       <span className={cx(isDisabled && "text-decoration-line-through")}>
         {option.url}
       </span>
-      {isDisabled && (
-        <span className="ms-1">
-          <XLg className={cx("bi", "me-1")} />
-          No public access
-        </span>
-      )}
+      {isDisabled &&
+        (option.data?.metadata?.visibility == "private" &&
+        !privateRepoBuildEnabled ? (
+          <span className="ms-1">
+            <XLg className={cx("bi", "me-1")} />
+            Private repository builds are not available
+          </span>
+        ) : (
+          <span className="ms-1">
+            <XLg className={cx("bi", "me-1")} />
+            No public access
+          </span>
+        ))}
     </>
   );
 }
