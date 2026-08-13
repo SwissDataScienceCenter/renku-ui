6104dae52f62e80497fd568ed99ab0f2398481f5
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2026-08-13 14:37:49 +0200

    feat: add persisted logs (#4284)
    Add support for persisted logs from Amalthea sessions and image builds. When persisted logs are enabled, get logs from the new persisted logs API endpoints and show the logs history.

--- client/server/constants.ts
+++ client/server/constants.ts
@@ -87,6 +87,8 @@ export const CONFIG_JSON = {
     process.env.BUILD_PRIVATE_REPO_BUILDS_ENABLED,
   CONTACT_EMAIL: process.env.CONTACT_EMAIL || "hello@renku.io",
   CULLING_THRESHOLDS: safeJsonToObject(process.env.CULLING_THRESHOLDS),
+  PERSISTED_LOGS_ENABLED: process.env.PERSISTED_LOGS_ENABLED,
+  PERSISTED_LOGS_TTL_SECONDS: process.env.PERSISTED_LOGS_TTL_SECONDS,
 };
 
 export const SAMPLE_PRIVACY_CONTENT = `# Privacy statement
